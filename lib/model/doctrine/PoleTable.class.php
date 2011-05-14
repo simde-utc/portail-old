a3d5bc4ba7056cb4539ce4c18a5e5b64bbe8091e
Thibault <thibault.roucou@gmail.com>
2011-05-14 15:41:04 +0200

    Correction d'un bug pour la récupération d'un pole par son login

--- lib/model/doctrine/PoleTable.class.php
+++ lib/model/doctrine/PoleTable.class.php
@@ -25,14 +25,18 @@ class PoleTable extends Doctrine_Table
      */
 		public static function retrieveByLogin($login)
 		{
-
-			$asso = AssoTable::retrieveByLogin($login);
 			
-	    $q = Doctrine_Query::create()
+			if($asso = AssoTable::retrieveByLogin($login))
+			{
+				$q = Doctrine_Query::create()
 	      ->from('Pole p');
-		 
-		  $q->Where('p.asso_id = ?', $asso->getId());
-		 
-		  return $q->fetchOne();
+			 
+			  $q->Where('p.asso_id = ?', $asso->getId());
+			 
+			  return $q->fetchOne();
+			}
+			else return null;
+			
+	    
 		}
 }

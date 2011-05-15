3c2f597a13ac5b06883935caf506d7b337a0c9b0
Matthieu Guffroy <mattgu74@gmail.com>
2011-05-15 21:32:28 +0200

    Ajout de la couleur dans le json pour etre afficher par le calendar
    Signed-off-by: Matthieu Guffroy <mattgu74@gmail.com>

--- apps/frontend/modules/event/templates/indexSuccess.json.php
+++ apps/frontend/modules/event/templates/indexSuccess.json.php
@@ -7,10 +7,11 @@ foreach ($events as $event)
   echo json_encode(
               array(
                 "id" => ($event->getId()),
-                "title" => ($event->getAsso(). " - " .$event->getName()), 
+                "title" => ($event->getAsso(). " - ".$event->getType()." - " .$event->getName()), 
                 "start" => ($event->getStartDate()),
                 "end" => ($event->getEndDate()),
-                "url" => url_for(array("module"=>"event", "action"=>"edit", "id" => $event->getId()))
+                "url" => url_for(array("module"=>"event", "action"=>"edit", "id" => $event->getId())),
+                "color" => ($event->getType()->getColor()),
                 )
               );
 }

598d8b325656f3701821ba650ebf220aa3bdc39f
Thomas Recouvreux <thomas.recouvreux@gmail.com>
2011-05-16 14:52:04 +0200

    layout list

---
+++ apps/frontend/modules/event/templates/listSuccess.php
@@ -0,0 +1,26 @@
+<?php use_stylesheet('event.css') ?>
+<?php use_helper('Text') ?>
+
+<h2><?php echo isset($asso) ? "Article de : ".$asso->getName() : "Liste des events" ?></h2>
+<!-- todo only if authorized -->
+<a href="<?php echo url_for('event/new') ?>">Créer un event</a>
+
+<ul id="event_list">
+  <?php foreach($events as $event) : ?>
+
+    <li>
+      <h3><?php echo $event->getName() ?></h3>
+  		Créé par <?php echo $event->getAsso()->getName() ?> le <?php echo $event->getCreatedAt() ?>  
+      <!-- todo only if authorized -->
+  		- <a href="<?php echo url_for('event/edit?id='.$event->getId()) ?>">Editer</a>
+
+      <img class="logo" src="<?php echo $event->getAsso()->getLogo() ?>">
+      <div class="desc">
+        <?php echo truncate_text($event->getDescription(),256) ?>
+        </br>
+        <a class="link" href="<?php echo url_for('event/show/?id='.$event->getId()) ?>">Lire la suite</a>
+      </div>
+    </li>
+
+  <?php endforeach; ?>
+</ul>

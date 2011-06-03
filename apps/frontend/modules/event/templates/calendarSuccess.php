c5db4dc47db7190df1e8c13d268833aac4aac98d
Arthur Puyou <arthur@puyou.fr>
2011-06-03 17:41:21 +0800

    Calendrier en français

--- apps/frontend/modules/event/templates/calendarSuccess.php
+++ apps/frontend/modules/event/templates/calendarSuccess.php
@@ -3,28 +3,53 @@
 
 <?php use_javascript('fullcalendar.min.js') ?>
 
-<div id='loading' style='display:none'>loading...</div>
-<div id='calendar'></div>
+<div id="loading" style="display:none">Chargement...</div>
+<div id="calendar"></div>
 <script>
-	$(document).ready(function() {
-	
-		$('#calendar').fullCalendar({
-      header: {
-          left: 'prev,next today',
-          center: 'title',
-          right: 'month,agendaWeek,agendaDay'
-        },
-			editable: false,
-      allDayDefault: false,
-			
-			events: "<?php echo url_for("/json/event/index") ?>",
-			
-			loading: function(bool) {
-				if (bool) $('#loading').show();
-				else $('#loading').hide();
-			}
-			
-		});
-		
-	});
+$(document).ready(function() {  
+  $("#calendar").fullCalendar({
+    header: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'month,agendaWeek,agendaDay'
+    },
+    editable: false,
+    allDayDefault: false,
+    events: "<?php echo url_for("/json/event/index") ?>",
+    loading: function(bool) {
+      if (bool) $('#loading').show();
+      else $('#loading').hide();
+    },
+    buttonText: {
+        today: 'aujourd&rsquo;hui',
+        month: 'mois',
+        week:  'semaine',
+        day:   'jour'
+      },
+    monthNames: ['janvier', 'février', 'mars', 'avril', 'mai',
+      'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'],
+    monthNamesShort: ['jan.', 'fév.', 'mar.', 'avr.', 'mai', 'juin',
+       'juil.', 'aoû.', 'sep.', 'oct.', 'nov.', 'déc.'],
+    dayNames: ['dimanche', 'lundi', 'mardi', 'mercredi',
+       'jeudi', 'vendredi', 'samedi'],
+    dayNamesShort: ['dim.', 'lun.', 'mar.', 'mer.', 'jeu.', 'ven.', 'sam.'],
+    firstDay: 1,
+    titleFormat: {
+      month: 'MMMM yyyy',
+      week: "d[ MMMM][ yyyy]{ - d MMMM yyyy}",
+      day: 'dddd d MMMM yyyy',
+    },
+    columnFormat: {
+      month: 'dddd',
+      week: 'ddd d',
+      day: '',
+    },
+    axisFormat: 'H:mm',
+    timeFormat: {
+      '': 'H:mm',
+      agenda: 'H:mm{ - H:mm}',
+    },
+    allDayText: 'Jour entier',
+  });
+});
 </script>

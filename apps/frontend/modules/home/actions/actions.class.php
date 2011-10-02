4bdda59bd8ab27ad4cc7af1b61fa1b4ca1b712bf
Chausse <laure@ubuntu.ubuntu-domain>
2011-10-02 16:37:34 +0200

    Ajout du module home

---
+++ apps/frontend/modules/home/actions/actions.class.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * home actions.
+ *
+ * @package    simde
+ * @subpackage home
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class homeActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->setTemplate('index');
+
+  }
+}

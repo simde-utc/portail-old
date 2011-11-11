ac3b583eea502bafe240c90824991e41751b3d0a
Matthieu Guffroy <mattgu74@gmail.com>
2011-11-11 23:11:05 +0100

    lien vers assos

--- apps/frontend/modules/article/templates/_lastArticles.php
+++ apps/frontend/modules/article/templates/_lastArticles.php
@@ -1,7 +1,7 @@
 <h1>Articles</h1>
 <?php foreach($articles as $article): ?>
 <div class="article">
-  <h2 style="background: <?php echo $article->getPole()->getCouleur()?>"><?php echo $article->getName() ?> - Par <?php echo $article->getAsso()->getName() ?> - Date <?php echo $article->getCreatedAt() ?></h2>
+  <h2 style="background: <?php echo $article->getPole()->getCouleur()?>"><?php echo $article->getName() ?> - Par <a href="<?php echo url_for('assos_show',$article->getAsso())?>"><?php echo $article->getAsso()->getName() ?></a> <span style="float: right;"><?php echo $article->getCreatedAt() ?></span></h2>
   <div class="article_content">
     <?php echo $article->getText() ?>
   </div>

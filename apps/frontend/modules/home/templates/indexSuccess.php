a6741b3eefb7e1ef5fc8a0996819bd57144edffd
Matthieu Guffroy <mattgu74@gmail.com>
2011-11-11 18:50:18 +0100

    css des events

--- apps/frontend/modules/home/templates/indexSuccess.php
+++ apps/frontend/modules/home/templates/indexSuccess.php
@@ -3,22 +3,22 @@
 </div>
 <div class="part" >
   <h1>Evénements</h1>
-  <div class="event">
-    PHOTO<br />
+  <div class="event poleae">
+    <img /><br />
     <h2>Titre event</h2>
     <div class="event_content">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
     </div>
   </div>
-  <div class="event">
-    PHOTO<br />
+  <div class="event polevdc">
+    <img /><br />
     <h2>Titre event</h2>
     <div class="event_content">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
     </div>
   </div>
-  <div class="event">
-    PHOTO<br />
+  <div class="event poleae">
+    <img /><br />
     <h2>Titre event</h2>
     <div class="event_content">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod

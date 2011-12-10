ec8ba1e685e6c89c333e8508a562e652a1f2d098
Emilien Kenler <hello@emilienkenler.com>
2011-12-10 17:57:04 +0100

    Trombi - close #596
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

--- apps/frontend/modules/asso/templates/_trombinoscope.php
+++ apps/frontend/modules/asso/templates/_trombinoscope.php
@@ -1,3 +1,18 @@
 <div id="trombi">
-  Trombi
+  <?php if($membres->count() > 0): ?>
+    <ul>
+      <?php foreach($membres as $membre) : ?>
+        <li class="membre"><?php echo $membre->getUser() ?>
+          <img src="https://demeter.utc.fr/pls/portal30/portal30.get_photo_utilisateur?username=<?php echo $membre->getUser()->getUsername() ?>" height="120" style="float: right;" />
+          <ul>
+            <li>Rôle : <?php echo $membre->getRole()->getName() ?> (<?php echo $membre->getSemestre() ?>)</li>
+            <li>Email : <?php echo $membre->getUser()->getEmailAddress() ?></li>
+            <li>Semestre : <?php echo $membre->getUser()->getProfile()->getBranche().'0'.$membre->getUser()->getProfile()->getSemestre() ?> <?php echo $membre->getUser()->getProfile()->getFiliere() ?></li>
+          </ul>
+          </li>
+      <?php endforeach; ?>
+    </ul>
+  <?php else: ?>
+    Cette association n'a pas de membre dans son bureau.
+  <?php endif ?>
 </div>

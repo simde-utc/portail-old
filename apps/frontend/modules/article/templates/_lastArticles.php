1b18414126608444213210d5b5d17fb2415b9ceb
Arthur Puyou <arthur@puyou.fr>
2011-12-10 19:09:59 +0100

    Html_entity_decode des articles sur la page d'accueil
    Html_entity_decode (http://php.net/manual/fr/function.html-entity-decode.php) pour virer les balises html dans les articles, mais c'est pas bien, c'est une idée de Matthieu et donc il faudra trouver une solution pour le virer plus tard.
    Cordialement,
    
    Signed-off-by: Arthur Puyou <arthur@puyou.fr>

--- apps/frontend/modules/article/templates/_lastArticles.php
+++ apps/frontend/modules/article/templates/_lastArticles.php
@@ -4,7 +4,7 @@
 <div class="article">
   <h2 style="background: <?php echo $article->getPole()->getCouleur()?>"><?php echo $article->getName() ?><span class="sub"><a href="<?php echo url_for('assos_show',$article->getAsso())?>" title="Voir la page de <?php echo $article->getAsso()->getName() ?>"><?php echo $article->getAsso()->getName() ?></a>, le <?php echo format_date($article->getCreatedAt(), 'P', 'fr'); ?></span></h2>
   <div class="article_content">
-    <?php echo $article->getText() ?>
+    <?php echo html_entity_decode($article->getText()) ?>
   </div>
 </div>
 <?php endforeach ?>

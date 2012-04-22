67395d4fcd6222ceed6fda7899c50fe7bbd9c935
Arthur Puyou <arthur@puyou.fr>
2012-04-23 01:22:21 +0200

    Suppression du TinyMCE des pages assos

--- lib/form/doctrine/AssoForm.class.php
+++ lib/form/doctrine/AssoForm.class.php
@@ -16,30 +16,6 @@ class AssoForm extends BaseAssoForm
         'name','pole_id','type_id','url_site','logo','summary','description','salle','phone','facebook'
     ));
     
-    $this->widgetSchema['description'] =  new sfWidgetFormTextareaTinyMCE(
-      array(
-        'width'=>520,
-        'height'=>150,
-        'config'=>'theme_advanced_disable: "anchor,image,cleanup,help"',
-        'theme'   =>  sfConfig::get('app_tinymce_theme','advanced'),
-      ),
-      array(
-        'class'   =>  'tiny_mce'
-      )
-    );
-    $this->widgetSchema['summary'] =  new sfWidgetFormTextareaTinyMCE(
-      array(
-        'width'=>520,
-        'height'=>80,
-        'config'=>'theme_advanced_disable: "anchor,image,cleanup,help"',
-        'theme'   =>  sfConfig::get('app_tinymce_theme','advanced'),
-      ),
-      array(
-        'class'   =>  'tiny_mce'
-      )
-    );
-    $js_path = '/js/tiny_mce/tiny_mce.js';
-    sfContext::getInstance()->getResponse()->addJavascript($js_path);
     
     $this->getWidget('pole_id')->setOption('query',PoleTable::getInstance()->getAllWithInfos());
   }

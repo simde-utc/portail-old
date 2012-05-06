156abd96c0726e1f257c2c65a2a02cacabac58bc
Arthur Puyou <arthur@puyou.fr>
2012-05-06 13:36:03 +0200

    Affichage des événements sur la page asso

--- apps/frontend/modules/asso/templates/_events.php
+++ apps/frontend/modules/asso/templates/_events.php
@@ -1,7 +1,7 @@
 <?php use_helper('Date') ?>
 <?php use_helper('Thumb') ?>
 <h1>
-  Nos événements
+  Prochains événements
   <?php if($sf_user->isAuthenticated() && $sf_user->getGuardUser()->hasAccess($asso->getLogin(), 0x08)): ?>
     <span class="titleaction"><i class="icon-plus icon-white"></i> <a href="<?php echo url_for('event_new', $asso) ?>">Ajouter un événement</a></span>
   <?php endif ?>
@@ -11,24 +11,22 @@
     <div id="event_list">
       <?php foreach($events as $event) : ?>
         <div class="event" style="background: <?php echo $event->getPole()->getCouleur() ?>">
-          <?php echo showThumb($event->getAffiche(), 'events', array('width'=>160, 'height'=>100), 'center') ?><br />
-          <h2><?php echo $event->getName() ?></h2>
-          Par <?php echo $event->getAsso()->getName() ?><br />
+          <a href="<?php echo url_for('event_show', $event) ?>"><?php echo showThumb($event->getAffiche(), 'events', array('width'=>160, 'height'=>100), 'center') ?></a><br />
+          <h2><a href="<?php echo url_for('event_show', $event) ?>"><?php echo $event->getName() ?></a></h2>
           Le <?php echo format_date($event->getStartDate(), 'd MMMM à HH:mm', 'fr'); ?>
           <p class="desc">
             <?php echo $event->getSummary() ?>
-            <a class="link" href="<?php echo url_for('event_show', $event) ?>">En savoir plus</a>
           </p>
           <?php if($sf_user->isAuthenticated() && $sf_user->getGuardUser()->hasAccess($event->getAsso()->getLogin(), 0x08)): ?>
             <div class="actions">
-              <a href="<?php echo url_for('event/edit?id=' . $event->getId()) ?>">Editer</a>
+                <i class="icon-edit icon-white"></i> <a href="<?php echo url_for('event/edit?id=' . $event->getId()) ?>">Éditer</a>
             </div>
           <?php endif ?>
         </div>
       <?php endforeach; ?>
     </div>
     <hr class="clear"/>
-    <a class="more"> >>> Voir tous les evenements</a>
+    <a class="btn">Voir tous les événements >></a>
   <?php else: ?>
     Cette association n'a pas encore proposé d'événement.
   <?php endif ?>

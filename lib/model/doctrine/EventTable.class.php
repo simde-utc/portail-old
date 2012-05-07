6235adf2ddc3314fa8f66eaa9bf4677a11c22aa1
Emilien Kenler <hello@emilienkenler.com>
2012-05-07 19:14:32 +0200

    Display articles and events only from Asso even it's a Pole
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

--- lib/model/doctrine/EventTable.class.php
+++ lib/model/doctrine/EventTable.class.php
@@ -29,10 +29,10 @@ class EventTable extends Doctrine_Table
       ->select('a.*')
       ->addOrderBy('a.created_at DESC');
 
-    if(!is_null($asso))
+    /*if(!is_null($asso))
       if($asso->isPole())
         $q = $q->leftJoin('Asso as')->where("as.pole_id = ?",$asso->getPrimaryKey());
-      else
+      else*/
         $q = $q->where("a.asso_id = ?",$asso->getPrimaryKey());
 
     return $q;

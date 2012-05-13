74e33664c3ec5b9f20cfea85bc80a00a3b01a70b
Arthur Puyou <arthur@puyou.fr>
2012-05-14 00:23:17 +0200

    Affichage des event

--- apps/frontend/modules/event/templates/showSuccess.php
+++ apps/frontend/modules/event/templates/showSuccess.php
@@ -2,23 +2,25 @@
 <?php use_helper('Date') ?>
 <?php use_helper('Thumb') ?>
 
-<h2><?php $event->getName() ?> </h2>
+<div class="part">
+  <h1>
+    <?php echo $event->getName() ?>
+    <?php if($sf_user->isAuthenticated() && $sf_user->getGuardUser()->hasAccess($event->getAsso()->getLogin(), 0x04)): ?>
+      <span class="titleaction"><i class="icon-edit icon-white"></i> <a href="<?php echo url_for('event/edit?id=' . $event->getId()) ?>">Éditer</a></span>
+    <?php endif ?>
+  </h1>
+  
+  <?php if($event->getAffiche()): ?>
+    <?php echo showThumb($event->getAffiche(), 'events', array('width'=>350, 'height'=>250, 'class'=>'affiche'), 'scale') ?><br />
+  <?php endif; ?>
 
-<h3><?php echo $event->getName() . " - " . format_date($event->getStartDate(), 'f', 'fr') . " au " . format_date($event->getEndDate(), 'f', 'fr'); ?></h3>
-Créé par <?php echo $event->getAsso()->getName() ?> le <?php echo format_date($event->getCreatedAt(), 'f', 'fr') ?>
-<br />
-Type: <?php echo $event->getType()->getName(); ?>
-<br />
-Lieu : <?php echo $event->getPlace(); ?>
-<br />
-<?php if ($sf_user->isAuthenticated() && $sf_user->getGuardUser()->hasAccess($event->getAsso()->getLogin(), 0x08)): ?>
-  - <a href="<?php echo url_for('event/edit?id=' . $event->getId()) ?>">Editer</a>
-<?php endif ?>
-<br/>
-<?php if($event->getAffiche()): ?>
-            <?php echo showThumb($event->getAffiche(), 'events', array('width'=>400, 'height'=>300), 'scale') ?><br />
-<?php endif; ?>
-<div class="desc">
-  <?php echo $event->getDescription(); ?>
-  </br>
+  <p>Du <?php echo format_date($event->getStartDate(), 'f', 'fr') ?> au <?php echo format_date($event->getEndDate(), 'f', 'fr') ?></p>
+
+  <p>
+    Créé par <a href="<?php echo url_for('assos_show',$event->getAsso())?>" title="Voir la page de <?php echo $event->getAsso()->getName() ?>"><?php echo $event->getAsso()->getName() ?></a><br />
+    Type : <?php echo $event->getType()->getName(); ?><br />
+    Lieu : <?php echo $event->getPlace(); ?>
+  </p>
+  <p><?php echo $event->getSummary() ?></p>
+  <p><?php echo $event->getDescription() ?></p>
 </div>

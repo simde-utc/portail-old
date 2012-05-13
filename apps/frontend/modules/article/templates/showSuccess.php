e549a8db2740fa85f6ffe215c4bd7e9560ac54eb
Arthur Puyou <arthur@puyou.fr>
2012-05-14 00:54:16 +0200

    Affichage des articles

--- apps/frontend/modules/article/templates/showSuccess.php
+++ apps/frontend/modules/article/templates/showSuccess.php
@@ -1,19 +1,19 @@
-<?php use_stylesheet('article.css') ?>
-<?php use_helper('Text') ?>
+<?php use_helper('Date') ?>
 <?php use_helper('Thumb') ?>
 
-<h2><?php $article->getName() ?> </h2>
+<div class="part">
+  <h1>
+    <?php echo $article->getName() ?>
+    <?php if($sf_user->isAuthenticated() && $sf_user->getGuardUser()->hasAccess($article->getAsso()->getLogin(), 0x04)): ?>
+      <span class="titleaction"><i class="icon-edit icon-white"></i> <a href="<?php echo url_for('article/edit?id=' . $article->getId()) ?>">Éditer</a></span>
+    <?php endif ?>
+  </h1>
+  
+  <?php if($article->getImage()): ?>
+    <?php echo showThumb($article->getImage(), 'articles', array('width'=>350, 'height'=>250, 'class'=>'affiche'), 'scale') ?><br />
+  <?php endif; ?>
 
-<h3><?php echo $article->getName() ?></h3>
-Publié par <?php echo $article->getAsso()->getName() ?> le <?php echo $article->getCreatedAt() ?>  
-<?php if ($sf_user->isAuthenticated() && $sf_user->getGuardUser()->hasAccess($article->getAsso()->getLogin(), 0x04)): ?>
-  - <a href="<?php echo url_for('article/edit?id=' . $article->getId()) ?>">Editer</a>
-<?php endif ?>
-<br/>
-<?php if($article->getImage()): ?>
-            <?php echo showThumb($article->getImage(), 'articles', array('width'=>400, 'height'=>300), 'scale') ?><br />
-<?php endif; ?>
-<div class="desc">
-  <?php echo $article->getText(); ?>
-  </br>
+  <p>Publié par <a href="<?php echo url_for('assos_show',$article->getAsso())?>" title="Voir la page de <?php echo $article->getAsso()->getName() ?>"><?php echo $article->getAsso()->getName() ?></a><br /></p>
+  <p><?php echo nl2br($article->getSummary()) ?></p>
+  <p><?php echo nl2br($article->getText()) ?></p>
 </div>

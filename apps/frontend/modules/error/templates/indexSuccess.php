e1dd84ca3017848c9e7a6cb27ef7ac45e6948a64
Arthur Puyou <arthur@puyou.fr>
2012-05-14 00:41:13 +0200

    Page 404 custom

---
+++ apps/frontend/modules/error/templates/indexSuccess.php
@@ -0,0 +1,5 @@
+<div class="part">
+  <h1>Une erreur s'est produite</h1>
+  <p>Désolé, l'URL que vous avez demandée n'existe pas.</p>
+  <p>Retrouvez l'association que vous recherchez grâce au portail des associations !</p>
+</div>

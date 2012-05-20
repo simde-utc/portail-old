1f64f1fc55efe1fc11c12714c5c1a2c7b1c2b137
Emilien Kenler <hello@emilienkenler.com>
2012-05-20 17:59:41 +0200

    backend semestre + event pic
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

---
+++ apps/backend/modules/semestre/lib/semestreGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * semestre module helper.
+ *
+ * @package    simde
+ * @subpackage semestre
+ * @author     Your name here
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class semestreGeneratorHelper extends BaseSemestreGeneratorHelper
+{
+}

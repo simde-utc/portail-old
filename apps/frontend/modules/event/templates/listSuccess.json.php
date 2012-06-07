4e53311907e4bee451b0f9d48b339133931724bb
Arthur Puyou <arthur@puyou.fr>
2012-06-07 16:40:16 +0200

    Affichage du nom de l'asso (et pas login) et osef du type

--- apps/frontend/modules/event/templates/listSuccess.json.php
+++ apps/frontend/modules/event/templates/listSuccess.json.php
@@ -6,7 +6,7 @@ foreach ($events as $event)
   if($i!=0) { echo ","; } else { $i++; }
   $arr = array(
     "id" => ($event->getId()),
-    "title" => ($event->getAsso(). " - ".$event->getType()." - " .$event->getName()), 
+    "title" => ($event->getAsso()->getName()." - " .$event->getName()), 
     "start" => (strtotime($event->getStartDate())),
     "end" => (strtotime($event->getEndDate())),
     "url" => url_for('event_show',$event),

65ce30ce3149ab1cefb62ef20ab8855706e8a261
Arthur Puyou <arthur@puyou.fr>
2012-06-07 16:43:10 +0200

    Nettoyage du code de génération du json

--- apps/frontend/modules/event/templates/listSuccess.json.php
+++ apps/frontend/modules/event/templates/listSuccess.json.php
@@ -1,20 +1,15 @@
-[<?php
+<?php
 $result = array();
-$i=0;
-foreach ($events as $event) 
-{
-  if($i!=0) { echo ","; } else { $i++; }
-  $arr = array(
+foreach ($events as $event){
+  $result[] = array(
     "id" => ($event->getId()),
     "title" => ($event->getAsso()->getName()." - " .$event->getName()), 
     "start" => (strtotime($event->getStartDate())),
     "end" => (strtotime($event->getEndDate())),
     "url" => url_for('event_show',$event),
-    "color" => ($event->getAsso()->getPole()->getCouleur())
-  );
-  if(strtotime($event->getEndDate()) - strtotime($event->getStartDate()) > 24*3600)
-    $arr["allDay"] = "true";
-    
-  echo json_encode($arr);
+    "color" => ($event->getAsso()->getPole()->getCouleur()),
+    "allDay" => (strtotime($event->getEndDate()) - strtotime($event->getStartDate()) > 24*3600) ? true : false
+  );  
 }
-?>]
+echo json_encode($result);
+?>

6a6539f3b2f2bf2d8710e909738c7a8280fec244
bart <bart@bart-T101MT.(none)>
2012-06-09 15:38:01 +0200

    tri entre les events, articles pour les flux
    Signed-off-by: bart <bart@bart-T101MT.(none)>

--- apps/frontend/modules/abonnement/templates/_myFlux.php
+++ apps/frontend/modules/abonnement/templates/_myFlux.php
@@ -3,13 +3,27 @@
   <h1>Mon Flux</h1>
   <?php if($abonnements && $abonnements->count() > 0): ?>
     <?php foreach ($abonnements as $abonnement):?>
-   <div class="all_abonnements">
+     <div class="all_abonnements">
      <b>  <a href="<?php echo url_for('article/show?id='.$abonnement['id']) ?>"><?php echo $abonnement['assoName'].' : '.$abonnement['name'] ?></b>
         <?php echo $abonnement['summary'] ?>
         <div class="barre"> </div>
    </div>
-   
-   
+  
+   <div class="events_abonnements">
+    <?php if($abonnement['article'] == 'event'): ?>   
+     <b>  <a href="<?php echo url_for('article/show?id='.$abonnement['id']) ?>"><?php echo $abonnement['assoName'].' : '.$abonnement['name'] ?></b>
+        <?php echo $abonnement['summary'] ?>
+        <div class="barre"> </div>
+     <?php endif; ?>  
+     </div> 
+     
+       <div class="articles_abonnements">
+    <?php if($abonnement['article'] == 'article'): ?>   
+     <b>  <a href="<?php echo url_for('article/show?id='.$abonnement['id']) ?>"><?php echo $abonnement['assoName'].' : '.$abonnement['name'] ?></b>
+        <?php echo $abonnement['summary'] ?>
+        <div class="barre"> </div>
+     <?php endif; ?>  
+     </div> 
     <?php endforeach; ?>
   <?php else: ?>
     Vous ne suivez aucune association.

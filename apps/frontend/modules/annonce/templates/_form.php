ac406cd16136e3406a8923d41407ba56253c029c
Emilien Kenler <hello@emilienkenler.com>
2012-06-12 14:51:18 +0200

    Annonces w/ filters - see #649
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

--- apps/frontend/modules/annonce/templates/_form.php
+++ apps/frontend/modules/annonce/templates/_form.php
@@ -2,25 +2,26 @@
 <?php use_javascripts_for_form($form) ?>
 
 <form class="well" action="<?php echo url_for('annonce/'.($form->getObject()->isNew() ? 'create' : 'update').(!$form->getObject()->isNew() ? '?id='.$form->getObject()->getId() : '')) ?>" method="post" <?php $form->isMultipart() and print 'enctype="multipart/form-data" ' ?>>
-<?php if (!$form->getObject()->isNew()): ?>
-<input type="hidden" name="sf_method" value="put" />
-<?php endif; ?>
+  <?php if(!$form->getObject()->isNew()): ?>
+    <input type="hidden" name="sf_method" value="put" />
+  <?php endif; ?>
   <table>
     <tfoot>
       <tr>
         <th></th>
         <td>
-          <?php echo $form->renderHiddenFields(false) ?>
-          &nbsp;<a href="<?php echo url_for('annonce/index') ?>">Back to list</a>
-          <?php if (!$form->getObject()->isNew()): ?>
-            &nbsp;<?php echo link_to('Delete', 'annonce/delete?id='.$form->getObject()->getId(), array('method' => 'delete', 'confirm' => 'Are you sure?')) ?>
+          <input class="btn btn-primary" type="submit" value="Save" />
+
+          &nbsp;<a href="<?php echo url_for('annonce/index') ?>" class="btn">Retour aux annonces</a>
+          <?php if(!$form->getObject()->isNew()): ?>
+            &nbsp;<?php echo link_to('Delete', 'annonce/delete?id='.$form->getObject()->getId(), array('method' => 'delete', 'confirm' => 'Are you sure?', 'class' => 'btn btn-danger')) ?>
           <?php endif; ?>
-          <input type="submit" value="Save" />
+
+          <?php echo $form->renderHiddenFields(false) ?>
         </td>
       </tr>
     </tfoot>
     <tbody>
-      <?php echo $form->renderGlobalErrors() ?>
       <?php echo $form ?>
     </tbody>
   </table>

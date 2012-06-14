83b52fe3477b0f0a4c1335571c3071983354ab33
Arthur Puyou <arthur@puyou.fr>
2012-06-14 19:22:36 +0200

    Amélioration des tabs

--- apps/frontend/modules/gesmail/templates/_liste.php
+++ apps/frontend/modules/gesmail/templates/_liste.php
@@ -1,27 +1,26 @@
 <ul class="nav nav-tabs">
-  <li class="dropdown<?php if($box->type == "alias") echo " active"; ?>">
+  <li class="dropdown<?php if(isset($box) && $box->type == "alias") echo " active"; ?>">
     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-        Redirections
+        <i class="icon-envelope"></i> Redirections
         <b class="caret"></b>
       </a>
     <ul class="dropdown-menu">
-      <li<?php if($box->getName() == $asso->getLogin()) echo ' class="active"'; ?>><a href="<?php echo url_for('gesmail', array('login' => $asso->getLogin())) ?>"><i class="icon-envelope"></i> <?php echo $asso->getLogin() ?></a></li>
+      <li<?php if(isset($box) && $box->getName() == $asso->getLogin()) echo ' class="active"'; ?>><a href="<?php echo url_for('gesmail', array('login' => $asso->getLogin())) ?>"> <?php echo $asso->getLogin() ?></a></li>
       <?php foreach($boxes['alias'] as $abox): ?>
-      <li<?php if($box->getName() == $abox->getName()) echo ' class="active"'; ?>><a href="<?php echo url_for('gesmail_box', array('box' => $abox->extension, 'login' => $asso->getLogin())) ?>"><i class="icon-envelope"></i> <?php echo $abox->getName() ?></a></li>
+      <li<?php if(isset($box) && $box->getName() == $abox->getName()) echo ' class="active"'; ?>><a href="<?php echo url_for('gesmail_box', array('box' => $abox->extension, 'login' => $asso->getLogin())) ?>"> <?php echo $abox->getName() ?></a></li>
       <?php endforeach; ?>
-      <li><a href="/gesmail/new/alias"><i class="icon-plus"></i> Ajouter</a></li>
     </ul>
   </li>
-  <li class="dropdown<?php if($box->type == "ml") echo " active"; ?>">
+  <li class="dropdown<?php if(isset($box) && $box->type == "ml") echo " active"; ?>">
     <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-        Mailing-listes
+        <i class="icon-list"></i> Mailing-listes
         <b class="caret"></b>
       </a>
     <ul class="dropdown-menu">
       <?php foreach($boxes['ml'] as $abox): ?>
-      <li<?php if($box->getName() == $abox->getName()) echo ' class="active"'; ?>><a href="<?php echo url_for('gesmail_box', array('box' => $abox->extension, 'login' => $asso->getLogin())) ?>"><i class="icon-list"></i> <?php echo $abox->getName() ?></a></li>
+      <li<?php if(isset($box) && $box->getName() == $abox->getName()) echo ' class="active"'; ?>><a href="<?php echo url_for('gesmail_box', array('box' => $abox->extension, 'login' => $asso->getLogin())) ?>"><?php echo $abox->getName() ?></a></li>
       <?php endforeach; ?>
-      <li><a href="/gesmail/new/ml"><i class="icon-plus"></i> Ajouter</a></li>
     </ul>
   </li>
+  <li><a href="<?php echo url_for('gesmail_create', array('login' => $asso->getLogin())) ?>"><i class="icon-plus"></i> Ajouter</a></li>
 </ul>

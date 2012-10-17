6156e69a86961b635a1a3b5a7c4ab34c02d53efa
Arthur Puyou <arthur@puyou.fr>
2012-10-17 23:44:58 +0100

    Inclusion de ginger

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -29,5 +29,8 @@ class ProjectConfiguration extends sfProjectConfiguration
     $this->enablePlugins('sfImageTransformPlugin');
     $this->enablePlugins('sfXssSafePlugin');
     sfForm::disableCSRFProtection();
+
+    require_once sfConfig::get('sf_lib_dir').'/vendor/ginger-client/Ginger.class.php';
+
   }
 }

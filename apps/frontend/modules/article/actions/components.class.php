33f9e8662842c3d616b30ff6b99856daebfff442
Arthur Puyou <arthur@puyou.fr>
2012-11-01 23:35:11 +0000

    On passe à 5 articles sur la page d'accueil

--- apps/frontend/modules/article/actions/components.class.php
+++ apps/frontend/modules/article/actions/components.class.php
@@ -4,6 +4,6 @@ class articleComponents extends sfComponents
 {
   public function executeLastArticles()
   {
-    $this->articles = ArticleTable::getInstance()->getLastArticles()->execute();
+    $this->articles = ArticleTable::getInstance()->getLastArticles(5)->execute();
   }
 }

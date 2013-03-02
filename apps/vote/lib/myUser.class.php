15c2680d2695d39b91145b4dd94527a1572c8f3e
Emilien Kenler <hello@emilienkenler.com>
2013-03-02 16:00:49 +0100

    Create user account if not exist
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

--- apps/vote/lib/myUser.class.php
+++ apps/vote/lib/myUser.class.php
@@ -13,10 +13,54 @@ class myUser extends sfGuardSecurityUser
     $username = phpCAS::getUser();
 
     $data = sfGuardUserTable::getInstance()->findOneBy('username', $username);
-    if($data && $data->getIsActive())
+    if(!$data || ($data->getPassword() == NULL && !$data->getIsActive()))
+        $data = $this->registerUser($username, $data);
+
+    if($data)
       $this->signin($data, true);
     else
       die('Unauthorized.');
+  }
+
+  private function registerUser($username, $data = NULL)
+  {
+    try {
+      $gingerKey = sfConfig::get('app_portail_ginger_key');
+
+      if($gingerKey != "abc"){
+        $ginger = new Ginger(sfConfig::get('app_portail_ginger_key'));
+        $cotisants = $ginger->getUser($username);
+      }
+      else {
+        $cotisants = new stdClass();
+        $cotisants->mail = $username."@etu.utc.fr";
+        $cotisants->prenom = "Le";
+        $cotisants->nom = "Testeur";
+        $cotisants->type = "etu";
+      }
+
+      if(!$data)
+        $data = new sfGuardUser();
+
+      $data->setUsername($username);
+      $data->setEmailAddress($cotisants->mail);
+      $data->setFirstName($cotisants->prenom);
+      $data->setLastName($cotisants->nom);
+      $data->setIsActive(true);
+      $data->save();
+
+      $profile = new Profile();
+      $profile->setUser($data);
+      $profile->setDomain($cotisants->type);
+      $profile->save();
+
+      return $data;
+    }
+    catch (ApiException $ex){
+      $this->setFlash('error', "Il n'a pas été possible de vous identifier. Merci de contacter simde@assos.utc.fr en précisant votre login et le code d'erreur ".$ex->getCode().".");          
+    }
+
+    return false;
   }
 
   /**

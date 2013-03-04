afc7cd1b3733ad632d33019528c1a01f1f188867
Emilien Kenler <hello@emilienkenler.com>
2013-03-04 12:31:23 +0100

    Disable VoteList:: edition with VoteListeForm
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

--- lib/form/doctrine/VoteListeForm.class.php
+++ lib/form/doctrine/VoteListeForm.class.php
@@ -12,6 +12,6 @@ class VoteListeForm extends BaseVoteListeForm
 {
   public function configure()
   {
-    unset($this['created_at'], $this['updated_at']);
+    unset($this['created_at'], $this['updated_at'], $this['count']);
   }
 }

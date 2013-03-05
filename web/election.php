ae0bf5add2eb7debd76076e20220563ab8d9df9d
Emilien Kenler <hello@emilienkenler.com>
2013-03-05 19:47:00 +0100

    Date d'ouverture des élections en dur
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

--- web/election.php
+++ web/election.php
@@ -1,5 +1,9 @@
 <?php
 
+if(date('dmY') !== "06032013") {
+	echo 'Il n\'y a pas d\'&eacute;lection aujourd\'hui, repassez plus tard.';
+	exit;
+}
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 

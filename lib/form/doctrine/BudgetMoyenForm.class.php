8d985f00c4bb996390fb22363e374b749f961d4f
Emilien Kenler <hello@emilienkenler.com>
2013-03-07 18:23:49 +0100

    Schema & Classes for treso tool
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

---
+++ lib/form/doctrine/BudgetMoyenForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * BudgetMoyen form.
+ *
+ * @package    simde
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class BudgetMoyenForm extends BaseBudgetMoyenForm
+{
+  public function configure()
+  {
+  }
+}

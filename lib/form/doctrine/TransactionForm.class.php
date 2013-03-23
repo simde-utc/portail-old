c005ae48a2be7ed016ff4f6b136322af0bb48939
Emilien Kenler <hello@emilienkenler.com>
2013-03-23 16:58:07 +0100

    N'affiche pas les comptes qui sont supprimés et qui ne sont pas à cette asso
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

--- lib/form/doctrine/TransactionForm.class.php
+++ lib/form/doctrine/TransactionForm.class.php
@@ -13,6 +13,15 @@ class TransactionForm extends BaseTransactionForm
   public function configure()
   {
       $this->widgetSchema['asso_id'] = new sfWidgetFormInputHidden();
+      $this->widgetSchema['compte_id'] = new sfWidgetFormDoctrineChoice(array(
+          'model' => $this->getRelatedModelName('CompteBanquaire'),
+          'query' => CompteBanquaireTable::getInstance()->getAllForAsso($this->getObject()->getAsso()),
+          'add_empty' => false
+      ));
+      $this->validatorSchema['compte_id'] = new sfValidatorDoctrineChoice(array(
+          'model' => $this->getRelatedModelName('CompteBanquaire'),
+          'query' => CompteBanquaireTable::getInstance()->getAllForAsso($this->getObject()->getAsso()),
+      ));
       unset($this['created_at'], $this['updated_at'], $this['deleted_at'], $this['note_de_frais_id'], $this['budget_poste_id']);
   }
 }

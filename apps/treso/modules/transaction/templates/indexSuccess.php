c8e42bb689966298e2f5d25149990043958ada75
Maxime <final7765@gmail.com>
2013-04-06 14:26:33 +0200

    Fin module transactions

--- apps/treso/modules/transaction/templates/indexSuccess.php
+++ apps/treso/modules/transaction/templates/indexSuccess.php
@@ -2,7 +2,7 @@
 
 <h1>Liste des transactions de l'association <?php echo $asso?> </h1>
 
-<table class="table table-bordered table-striped">
+<table style="margin: 20px auto;" class="table table-bordered table-striped">
   <thead>
     <?php if(TransactionTable::getInstance()->getActiveCount($asso) > 20) {?>
       <a href="<?php echo url_for('transaction_new', $asso) ?>" class="btn btn-success"><i class="icon-plus icon-black"></i>&nbsp;&nbsp;Nouvelle transaction</a>
@@ -10,7 +10,8 @@
     <tr>
       <th>Compte</th>
       <th>Libelle</th>
-      <th>Montant</th>
+      <th>Crédit</th>
+      <th>Débit</th>  
       <th>Commentaire</th>
       <th>Date transaction</th>
       <th>Date rapprochement</th>
@@ -21,18 +22,25 @@
   </thead>
   <tbody>
     <?php foreach ($transactions as $transaction): ?>
-    <tr>
+    <?php if($transaction->getMontant() >= 0) : ?>
+    <tr class="recette">
+      <?php else : ?>
+    <tr class="depense">
+      <?php endif; ?>
       <td><?php echo $transaction->getCompteBanquaire() ?></td>
       <td><?php echo $transaction->getLibelle() ?></td>
-      <td><?php echo $transaction->getMontant() ?></td>
+      <td><?php if($transaction->getMontant() >= 0) echo $transaction->getMontant(); ?></td>
+      <td><?php if($transaction->getMontant() < 0) echo $transaction->getMontant(); ?></td>
       <td><?php echo $transaction->getCommentaire() ?></td>
       <td><?php echo format_date($transaction->getDateTransaction(), 'D', 'fr'); ?></td>
       <td><?php echo format_date($transaction->getDateRapprochement(), 'D', 'fr'); ?></td>
       <td><?php echo $transaction->getTransactionMoyen() ?></td>
       <td><?php echo $transaction->getMoyenCommentaire() ?></td>
       <td>
-          <a href="<?php echo url_for('transaction/edit?id='.$transaction->getId()) ?>" class="btn"><i class="icon-pencil"></i>&nbsp;&nbsp;Editer</a>
-          <?php echo link_to('<i class="icon-trash icon-white"></i>&nbsp;&nbsp;Supprimer', 'transaction/delete?id='.$transaction->getId(),  array('method' => 'delete', 'confirm' => 'Voulez-vous vraiment supprimer la transaction "'.$transaction->getLibelle().'" ?', 'class' => 'btn btn-danger')) ?>
+          <div class="btn-group">
+            <a href="<?php echo url_for('transaction/edit?id='.$transaction->getId()) ?>" class="btn"><i class="icon-pencil"></i>&nbsp;&nbsp;Editer</a>
+            <?php echo link_to('<i class="icon-trash icon-white"></i>', 'transaction/delete?id='.$transaction->getId(),  array('method' => 'delete', 'confirm' => 'Voulez-vous vraiment supprimer la transaction "'.$transaction->getLibelle().'" ?', 'class' => 'btn btn-danger')) ?>
+          </div>
       </td>
     </tr>
     <?php endforeach; ?>
--- lib/form/doctrine/TransactionForm.class.php
+++ lib/form/doctrine/TransactionForm.class.php
@@ -22,6 +22,36 @@ class TransactionForm extends BaseTransactionForm
           'model' => $this->getRelatedModelName('CompteBanquaire'),
           'query' => CompteBanquaireTable::getInstance()->getAllForAsso($this->getObject()->getAsso()),
       ));
+      
+      $this->validatorSchema['debit'] = new sfValidatorBoolean();
+      
       unset($this['created_at'], $this['updated_at'], $this['deleted_at'], $this['note_de_frais_id'], $this['budget_poste_id']);
+      
+      $this->widgetSchema['debit'] = new sfWidgetFormInputCheckbox(array(),array('style' => 'display : inline-block;'));
+      $this->setDefault('debit',true);
+      
+      $this->getWidgetSchema()->setPositions(array(
+         'id',
+         'asso_id',
+         'compte_id',
+         'libelle',
+         'debit',
+         'montant',
+         'commentaire',
+         'date_transaction',
+         'date_rapprochement',
+         'date_rapprochement',
+         'moyen_id',
+         'moyen_commentaire'));
+      
+      $this->validatorSchema['moyen_commentaire']->setOption('required', false);
+  }
+  
+  public function processValues($values)
+  {
+      $isCheckedDebit = $values['debit'];
+      if($isCheckedDebit)
+          $values['montant'] = - $values['montant'];
+      return parent::processValues($values);
   }
 }

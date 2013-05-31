d887a7f3ea09632814f6b5e5c9a51620b8676bcc
MonsieurV <MonsieurV@server.fake>
2013-05-31 19:02:48 +0200

    Ajouter un lien vers l'annonce depuis le backend (fin)

--- apps/bde/modules/infojob_offre/templates/_lien.php
+++ apps/bde/modules/infojob_offre/templates/_lien.php
@@ -1,3 +1,4 @@
 <?php
 use_helper('CrossLink');
-echo '<a href="' . cross_app_link_to('frontend', '@infojob_offre_show', array('id' => '1')) .'" target="_blank">Voir l\'annonce</a>';
+echo '<a href="' . cross_app_link_to('frontend', '@infojob_offre_show', array('id' => $info_job_offre->getId())) .'" target="_blank">Voir l\'annonce</a>';
+

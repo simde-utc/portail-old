a194004a1adcb371f20d80204e0084d00f2b289e
souki1990 <soukaina.bouaziz@gmail.com>
2013-06-01 22:58:55 +0200

    css

--- apps/frontend/modules/infojob/templates/_showcontact.php
+++ apps/frontend/modules/infojob/templates/_showcontact.php
@@ -1,6 +1,10 @@
 <?php if($sf_user->isAuthenticated()): ?>
+
+
   <!-- TODO -->
 <?php else: ?>
-  <a href="http://portail.local/frontend_dev.php/infojob/offres" class="btn"><i class="icon-arrow-left"></i> Retour</a>
-  <a href="http://portail.local/frontend_dev.php/infojob/new" class="btn btn-warning">Connectez-vous <i class="icon-white icon-share-alt"></i></a>
+
+  <a href="http://portail.local/frontend_dev.php/infojob/offres" class="btn-gris"><i class="icon-arrow-left"></i> Retour</a>
+ <a href="http://portail.local/frontend_dev.php/infojob/new" class="btn-jaune">Connectez-vous <i class="icon-white icon-share-alt"></i></a>
+ 
 <?php endif; ?>

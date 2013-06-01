3809194fda0ce68e78a9b068c6e7c39c195f54b4
souki1990 <soukaina.bouaziz@gmail.com>
2013-06-01 21:11:46 +0200

    rien

--- apps/frontend/modules/infojob/templates/showSuccess.php
+++ apps/frontend/modules/infojob/templates/showSuccess.php
@@ -1,6 +1,6 @@
 <div class="part" >
   <?php include_partial('infojob/topbar') ?>
-  <div class="well">
+  <div class="well"  style="border:1 px grey solid">
     <?php if($sf_user->isAuthenticated() && $annonce->getUserId() == $sf_user->getGuardUser()->getId()): ?>
       <a href="<?php echo url_for('annonce/edit?id=' . $annonce->getId()) ?>"  style="float:left;margin-top:12px; margin-right: 5px;"><i class="icon-pencil"></i></a>
     <?php endif ?>
@@ -8,7 +8,7 @@
     <p><?php echo $annonce->getTexte() ?></p>
     <p>
       <?php if($annonce->getRemuneration() != '0.00'): ?>Prix : <?php echo $annonce->getRemuneration() ?>€<br /><?php endif ?>
-      <?php if($annonce->getLieu()): ?>Lieu : <?php echo $annonce->getLieu() ?><br /><?php endif ?>
+      <?php if($annonce->getLieu()): ?><b>Lieu:</b> <?php echo $annonce->getLieu() ?><br /><?php endif ?>
     </p>
     <p style="font-style: italic;">
       Posté le <?php echo $annonce->getCreatedAt() ?>

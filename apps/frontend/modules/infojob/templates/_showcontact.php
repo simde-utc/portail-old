d2d72fbcaad62f025678851292a907cc8281a39c
souki1990 <soukaina.bouaziz@gmail.com>
2013-06-01 21:09:01 +0200

    quelquesmodifs

--- apps/frontend/modules/infojob/templates/_showcontact.php
+++ apps/frontend/modules/infojob/templates/_showcontact.php
@@ -1,5 +1,6 @@
 <?php if($sf_user->isAuthenticated()): ?>
   <!-- TODO -->
 <?php else: ?>
-  <p>Connectez-vous pour voir le contact.</p>
+  <a href="http://portail.local/frontend_dev.php/infojob/offres" class="btn"><i class="icon-arrow-left"></i> Retour</a>
+  <a href="http://portail.local/frontend_dev.php/infojob/new" class="btn btn-warning">Connectez-vous <i class="icon-white icon-share-alt"></i></a>
 <?php endif; ?>

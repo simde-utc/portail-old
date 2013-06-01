d027ccf43e21503d6cc88a354bd6dcf1686044b1
souki1990 <soukaina.bouaziz@gmail.com>
2013-06-01 21:09:22 +0200

    topbar

--- apps/frontend/modules/infojob/templates/_topbar.php
+++ apps/frontend/modules/infojob/templates/_topbar.php
@@ -1,1 +1,18 @@
 <!-- Menu du haut commun au service infojob TODO, voir exemple dans apps/frontend/modules/assos/templates/_topbar.php -->
+<div class="well">
+<div class="row-fluid">
+  <div class="span4" style="height:100px;">
+  <a href="#" class="btn-job">Voir les offres</a>
+  </div>
+  <div class="span4" style="height:100px">
+ 
+  <a href="#" class="btn-offres">Déposer une offre</a>
+  <span class="span-job">Blabla</span>
+  
+  </div>
+  
+  <div class="span4" style="height:100px">
+<a href="#" class="btn-comptes">Gérer mon compte</a></div>
+
+</div>
+</div>

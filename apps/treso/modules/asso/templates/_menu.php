f6d13d05f9068ec96e4312212908ba9e24fc16be
Emilien Kenler <hello@emilienkenler.com>
2013-06-02 14:10:07 +0200

    Liste des chèques
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

--- apps/treso/modules/asso/templates/_menu.php
+++ apps/treso/modules/asso/templates/_menu.php
@@ -4,6 +4,7 @@
         <li class="nav-header"><?php echo $asso->getName() ?></li>
         <li><a href="<?php echo url_for('budget_list',$asso) ?>">Budget Prévisionnel</a></li>
         <li><a href="<?php echo url_for('transaction',$asso) ?>">Livre de compte</a></li>
+        <li><a href="<?php echo url_for('cheque_list',$asso) ?>">Liste des chèques</a></li>
         <li><a href="<?php echo url_for('compte',$asso) ?>">Comptes bancaires</a></li>
         <li><a href="<?php echo url_for('ndf',$asso) ?>">Notes de frais</a></li>
         <?php endif ?>
---
+++ apps/treso/modules/cheque/actions/actions.class.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * cheque actions.
+ *
+ * @package    simde
+ * @subpackage cheque
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class chequeActions extends tresoActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->asso = $this->getRoute()->getObject();
+    $this->checkAuthorisation($this->asso);
+
+    $this->cheques = TransactionTable::getInstance()->getChequesEmis($this->asso)->execute();
+    $this->getResponse()->setSlot('current_asso', $this->asso);
+  }
+}

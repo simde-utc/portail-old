35a353a267294ed0e49b2f3078fd2ee99359018a
MaureenUTC <maureen@maureenPC.(none)>
2013-06-09 12:20:57 +0200

    contenu : presentation

--- apps/frontend/modules/infojob/templates/indexSuccess.php
+++ apps/frontend/modules/infojob/templates/indexSuccess.php
@@ -2,7 +2,10 @@
   <?php include_partial('infojob/topbar') ?>
   <h1>InfoJob, le service emploi du BDE-UTC</h1>
   <div class="well">
-    <p>Bienvenue sur le service InfoJob du BDE-UTC. Ici mettre le texte de présentation et la vidéo. TODO</p>
+        <p>Le BDE aide à la recherche d’emploi les étudiants UTC!
+Cet espace permet de mettre en relation les entreprises et particuliers avec des étudiants de l’UTC.
+Les étudiants peuvent consulter des annonces d’emplois et y postuler en un seul clic!
+Les employeurs pourront déposer leurs offres d’emploi et trouver un étudiant rapidement.</p>
   </div>
   <a href="<?php echo url_for('infojob/new') ?>" style="color:#FFF" class="btn btn-primary btn-large">Poster une offre d'emploi</a><br />
   <br />

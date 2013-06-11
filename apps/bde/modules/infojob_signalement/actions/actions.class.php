6fe22de9c6ae0806bfa098f766d12f04997b7597
MonsieurV <MonsieurV@server.fake>
2013-06-11 16:56:18 +0200

    Gérer les signalements dans le back-office. TODO : pouvoir invalider ou valider le signalement (dans le dernier cas, archive l'offre)

---
+++ apps/bde/modules/infojob_signalement/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/infojob_signalementGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/infojob_signalementGeneratorHelper.class.php';
+
+/**
+ * infojob_signalement actions.
+ *
+ * @package    simde
+ * @subpackage infojob_signalement
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class infojob_signalementActions extends autoInfojob_signalementActions
+{
+}
---
+++ apps/bde/modules/infojob_signalement/templates/_lien.php
@@ -0,0 +1,3 @@
+<?php use_helper('CrossLink'); ?>
+<a href="<?php cross_app_link_to('frontend', '@infojob_offre_show', array('id' => $info_job_signalement->getoffreId())) ?>" target="_blank">Voir l'annonce</a>
+

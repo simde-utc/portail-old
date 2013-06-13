f7a5d85f01661d26e1104c940635457678d8222a
souki1990 <soukaina.bouaziz@gmail.com>
2013-06-13 13:49:03 +0200

    signaler offre

--- apps/frontend/modules/infojob/templates/monprofilSuccess.php
+++ apps/frontend/modules/infojob/templates/monprofilSuccess.php
@@ -4,14 +4,32 @@
 <div class="container-fluid">
    <div class="row-fluid"style="margin-right:3%;">
       <div class="span12"style=" height:500px;boder:1px grey solid">
-     	<div class="well">
-	<p>Je souhaite recevoir des offres d'emploi par email. </p>
-	<p>Voici mes préférences : </p>
-	  <?php include_partial('form', array('form' => $form)) ?>
-	  <?php include_partial('form', array('form' => $form2)) ?>
-	</div>
-	   </div>
-   	 </div>
+         <div class="well">
+    <p>Je souhaite recevoir des offres d'emploi par email. </p>
+    <p>Voici mes préférences : </p>
+     <form name="myform" action="http://www.mydomain.com/myformhandler.cgi" method="POST">
+<div>Catégorie:<ul>
+<input type="checkbox" name="option1" value="Soutien Scolaire"> Soutien Scolaire<br>
+<input type="checkbox" name="option2" value="Aide à domicile" checked>Aide à domicile<br>
+<input type="checkbox" name="option3" value="Restauration"> Restauration<br>
+<input type="checkbox" name="option1" value="Manutention"> Manutention<br>
+<input type="checkbox" name="option2" value="Divers" checked> Divers<br>
+</ul>
+
+Disponibilité:<ul>
+<input type="checkbox" name="option1" value="Semaine">Semaine<br>
+<input type="checkbox" name="option2" value="Week-end" checked>Week-end<br>
+<input type="checkbox" name="option3" value="Soirs">Soirs<br>
+<input type="checkbox" name="option1" value="Vacances">Soirs<br>
+</ul>
+<input type="button" name="save" value="Enregistrer">
+</div>
+
+
+</form>
+    </div>
+       </div>
+        </div>
    </div>
 
 </div>

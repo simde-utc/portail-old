5d61417807fd24c5a80fe7130a7d7cfba57ca20b
MonsieurV <MonsieurV@server.fake>
2013-06-21 19:22:57 +0200

    Modification de l'email

--- apps/frontend/modules/infojob/templates/_validationemail.php
+++ apps/frontend/modules/infojob/templates/_validationemail.php
@@ -1,23 +1,3 @@
-<!doctype html>
-<html lang="fr">
-<head>
-  <meta charset="utf-8">
-</head>
-<body>
-  <div>
-    <img src="<?php echo public_path('images/logo_bde.png', true); ?>" alt="BDE UTC" width="163px" height="110px">
-  </div>
-  <div>
-  	<h1>Merci d&#39;avoir choisi InfoJob ! </h1>
-    <p>Votre offre a bien été enregistré sur le service InfoJob du portail des associations de l'UTC. Afin de valider la publication de votre offre, veuillez cliquez sur le lien suivant : </p>
-    <a href="<?php echo url_for('infojob/activate?key=' . $annonce->getEmailkey(), true); ?>"><?php echo url_for('infojob/activate?key=' . $annonce->getEmailkey(), true); ?></a>
-  </div>
-  <div>
-    <p>Vous pouvez à tout moment gérer votre annonce en cliquant sur le lien suivant : </p><a href="<?php echo url_for('infojob/edit?key=' . $annonce->getEmailkey(), true); ?>"><?php echo url_for('infojob/edit?key=' . $annonce->getEmailkey(), true); ?></a>
-  </div>
-</body>
-</html>
-
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>

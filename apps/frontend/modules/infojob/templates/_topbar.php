a2a4d1d1686b7063fe5de689992da71efc55f96e
MonsieurV <MonsieurV@server.fake>
2013-06-22 03:40:21 +0200

    Détourer les images, c'est quand même mieux.

--- apps/frontend/modules/infojob/templates/_topbar.php
+++ apps/frontend/modules/infojob/templates/_topbar.php
@@ -2,14 +2,14 @@
   <div class="row-fluid" style="text-align:center;">
     <div class="span6">
     	<h1>Etudiants</h1>
-    	<div><a href="<?php echo url_for('infojob/offres') ?>#infojob-annonces"><img src="/images/icones/job.jpg" style="witdh:90px; height:70px;"></a></div>
+    	<div><a href="<?php echo url_for('infojob/offres') ?>#infojob-annonces"><img src="/images/icones/job.png" style="witdh:90px; height:70px;"></a></div>
       <br/>
     	<div><a href="<?php echo url_for('infojob/offres') ?>#infojob-annonces" class="btn btn-success" style="color: #FFFFFF;">Voir les offres</a></div>
     </div>
     
     <div class="span6">
       <h1>Entreprise et particuliers</h1>
-      <div><a href="<?php echo url_for('infojob/new') ?>"><img src="/images/icones/entreprise.jpg" style="line-height:normal;witdh:90px; height:70px;"></a></div>
+      <div><a href="<?php echo url_for('infojob/new') ?>"><img src="/images/icones/entreprise.png" style="line-height:normal;witdh:90px; height:70px;"></a></div>
 	    <br/>
 	   	<div><a href="<?php echo url_for('infojob/new') ?>" class="btn btn-warning" style="color: #FFFFFF;">Déposer une offre</a></div>
     </div>

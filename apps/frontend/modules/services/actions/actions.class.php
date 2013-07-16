dd5bee3efaf6bb07c6c63b99731c2bfdda840921
Emilien Kenler <hello@emilienkenler.com>
2013-07-16 12:06:12 +0200

    Cleanup services
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

--- apps/frontend/modules/services/actions/actions.class.php
+++ apps/frontend/modules/services/actions/actions.class.php
@@ -9,16 +9,8 @@
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
 class servicesActions extends sfActions {
-    /**
-     * Executes index action
-     *
-     * @param sfRequest $request A request object
-     */
-    public function executeIndex(sfWebRequest $request) {
-        $this->forward('default', 'module');
-    }
 
-    public function executeShow(sfWebRequest $request) {
+    public function executeIndex(sfWebRequest $request) {
         $this->services = ServiceTable::getInstance()->getServicesList()->execute();
     }
 

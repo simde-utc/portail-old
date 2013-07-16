946dea31db027214e46a71e35d05367af98c5fdf
Emilien Kenler <hello@emilienkenler.com>
2013-07-16 19:53:46 +0200

    BDE service form user friendly
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

--- lib/form/doctrine/ServiceForm.class.php
+++ lib/form/doctrine/ServiceForm.class.php
@@ -12,6 +12,30 @@ class ServiceForm extends BaseServiceForm
 {
   public function configure()
   {
+    sfProjectConfiguration::getActive()->loadHelpers(array('Asset', 'Thumb'));
+
     unset($this['created_at'], $this['updated_at']);
+
+    $this->widgetSchema['logo'] = new sfWidgetFormInputFileEditable(array(
+      'file_src' => doThumb($this->getObject()->getLogo(), 'services', array('width'=>150, 'height'=>150), 'scale'),
+      'is_image' => true,
+      'edit_mode' => (!$this->isNew() && $this->getObject()->getLogo()),
+      'with_delete' => true,
+      'delete_label' => "Supprimer ce logo",
+    ));
+
+    $this->validatorSchema['logo'] = new sfValidatorFileImage(array(
+      'required' => false,
+      'path' => sfConfig::get('sf_upload_dir').'/services/source',
+      'mime_types' => 'web_images',
+      'max_width' => 1000,
+      'max_height' => 1000
+    ));
+
+    $this->validatorSchema['logo_delete'] = new sfValidatorBoolean();
+
+    $this->widgetSchema['nom']->setAttribute('size','50');
+    $this->widgetSchema['resume']->setAttribute('size','50');
+    $this->widgetSchema['url']->setAttribute('size','50');
   }
 }

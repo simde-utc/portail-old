aa3084d50476db8116d500f3dd2f9e68db47a028
Corentin <corentin.bras@etu.utc.fr>
2013-07-18 19:53:18 +0200

    ptite erreur

--- apps/frontend/modules/locaux/templates/locauxCtrlSuccess.php
+++ apps/frontend/modules/locaux/templates/locauxCtrlSuccess.php
@@ -8,7 +8,7 @@
         <?php /*Attention ça pique aux yeux*/
           if (isset($_POST["porte_mde"]) && $_POST["porte_mde"]==1) echo "<li>Porte de la MDE</li>";
           if (isset($_POST["bat_a"]) && $_POST["bat_a"]==1) echo "<li>Batiment A</li>";
-          if (isset($_POST["mde_complete"]) && $_POST["porte_mde"]==1) echo "<li>MDE complète</li>";
+          if (isset($_POST["mde_complete"]) && $_POST["mde_complete"]==1) echo "<li>MDE complète</li>";
           if (isset($_POST["locaux_pic"]) && $_POST["locaux_pic"]==1) echo "<li>Locaux du PIC</li>";
           if (isset($_POST["bureau_polar"]) && $_POST["bureau_polar"]==1) echo "<li>Bureau du Polar</li>";
           if (isset($_POST["perm_polar"]) && $_POST["perm_polar"]==1) echo "<li>Permanence du Polar</li>";

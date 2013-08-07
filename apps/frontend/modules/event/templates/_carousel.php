083c6c5a6b534fbbe198c087381a97feedc7fc4d
Arthur Puyou <arthur@puyou.fr>
2013-08-07 11:51:20 +0200

    Suppression caroussel

--- apps/frontend/modules/event/templates/_carousel.php
+++ apps/frontend/modules/event/templates/_carousel.php
@@ -1,21 +1,27 @@
 <?php use_helper('Date') ?>
 <?php use_helper('Thumb') ?>
 
-<!-- Carousel from : http://tympanus.net/codrops/2011/08/16/circular-content-carousel/ -->
-<div id="calendrier">
-  <h1>Prochains événements</h1>
-  <div class="wrap ca-wrapper">
-    <?php foreach($events as $event) : ?>
-      <div class="event ca-item">
-          <a href="<?php echo url_for('event/show?id='.$event->getId()) ?>"><?php echo showThumb($event->getAffiche(), 'events', array('width'=>112, 'height'=>112), 'center') ?></a>
-          <h3><a href="<?php echo url_for('event/show?id='.$event->getId()) ?>"><?php echo $event->getName() ?></a><br /></h3>
-          <p><?php echo $event->getSummary() ?></p>
-          <p>Par <a href="<?php echo url_for('asso/show?login='.$event->getAsso()->getLogin()) ?>" title="<?php echo $event->getAsso()->getName() ?>"><?php echo $event->getAsso()->getName() ?></a><br />
-          <?php echo format_date($event->getStartDate(),"D",'fr') ?>, <?php echo format_date($event->getStartDate(),"t",'fr') ?></p>
-      </div>
-    <?php endforeach; ?>
-  </div>
+<div class="row">
+    <div class="span12">
+        <h1 class="partie">Prochains événements</h1>
+    </div>
 </div>
-<script>
-  $('#calendrier').contentcarousel();
-</script>
+<div id="calendrier" class="row">
+    <?php foreach($events as $event) : ?>
+        <div class="event span4">
+            <div class="media">
+                <a class="pull-left" href="<?php echo url_for('event/show?id='.$event->getId()) ?>">
+                    <?php echo showThumb($event->getAffiche(), 'events', array('width'=>112, 'height'=>112, 'class'=>'media-object'), 'center') ?>
+                </a>
+                <div class="media-body">
+                    <h4 class="media-heading"><a href="<?php echo url_for('event/show?id='.$event->getId()) ?>"><?php echo $event->getName() ?></a></h4>
+                    <p><?php echo $event->getSummary() ?></p>
+                    <p>
+                        Par <a href="<?php echo url_for('asso/show?login='.$event->getAsso()->getLogin()) ?>" title="<?php echo $event->getAsso()->getName() ?>"><?php echo $event->getAsso()->getName() ?></a><br />
+                        <?php echo format_date($event->getStartDate(),"D",'fr') ?>, <?php echo format_date($event->getStartDate(),"t",'fr') ?>
+                    </p>
+                </div>
+            </div>
+        </div>
+    <?php endforeach; ?>
+</div>

9558d97ca30cfc8399d8aed268381f5512d1cd23
Arthur Puyou <arthur@puyou.fr>
2013-08-07 18:57:52 +0200

    Ask for specific number of events

--- apps/frontend/modules/event/actions/components.class.php
+++ apps/frontend/modules/event/actions/components.class.php
@@ -4,11 +4,11 @@ class eventComponents extends sfComponents
 {
   public function executeCarousel()
   {
-    $this->events = EventTable::getInstance()->getFutureEventsList(20)->execute();
+    $this->events = EventTable::getInstance()->getFutureEventsList(3)->execute();
   }
   
   public function executeLastEvents()
   {
-    $this->events = EventTable::getInstance()->getLastEvents()->execute();
+    $this->events = EventTable::getInstance()->getLastEvents(5)->execute();
   }
 }

abe397aea807a2fc4a6f9338c64fc7704415fd0f
Corentin <corentin.bras@etu.utc.fr>
2013-08-20 15:45:12 +0200

    Interface créer automatiquement pour le bde et l'admin

---
+++ apps/backend/modules/charte_locaux/actions/actions.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once dirname(__FILE__).'/../lib/charte_locauxGeneratorConfiguration.class.php';
+require_once dirname(__FILE__).'/../lib/charte_locauxGeneratorHelper.class.php';
+
+/**
+ * charte_locaux actions.
+ *
+ * @package    simde
+ * @subpackage charte_locaux
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class charte_locauxActions extends autoCharte_locauxActions
+{
+}

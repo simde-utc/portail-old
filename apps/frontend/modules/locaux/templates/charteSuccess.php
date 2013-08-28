edfbde3337ebf0d488a9b488790173b0c9d2939f
jofroid <geoffroydietsch@gmail.com>
2013-08-28 15:36:08 +0200

    On remplace Blabla Charte locaux par la charte d'accès étendue.

--- apps/frontend/modules/locaux/templates/charteSuccess.php
+++ apps/frontend/modules/locaux/templates/charteSuccess.php
@@ -1,6 +1,34 @@
 <div class="part">
-  <h1>Charte locaux</h1>
-  <p> Blablabla Voici la charte locaux blabla</p>
+  <h1>Charte d'accès étendu</h1>
+  <h2>Charte d’accès étendu à la Maison des Etudiants</h2>
+  
+<p> L’accès à la Maison des Etudiants <u><font color="#AA0000">(MDE) EST LIBRE DE 7 H30 À 23H 00 DU LUNDI AU VENDREDI, HORS
+PÉRIODE DE VACANCES , ET LES WEEK - ENDS ET JOURS FÉRIÉ. P OUR LE BÂTIMENT A, LES HORAIRES SONT
+LES MÊMES QU ’INDIQUÉES DANS LE RI DE L ’UTC.</font></u></p>
+
+<p>L’activation de votre badge sésame (carte d’étudiant) pour l’accès étendu vous garantira l’accès à la MDE
+(semaine, week-end et vacances) suivant le groupe d’accès demandé (un seul groupe d'accès possible).</p>
+
+L’étudiant détenteur d’un badge sésame lui donnant l’accès étendu à la MDE s’engage à :
+<ul> 
+<li>Ne pas prêter son badge sésame,</li>
+<li>Signaler toute perte de son badge sésame auprès du BDE-UTC et de l’UTC dans les meilleurs
+délais,</li>
+<li>Pénétrer dans la MDE uniquement dans le cadre de l’activité associative,</li>
+<li>Vérifier que tous les accès (portes et fenêtres) sont bien fermés à son départ,</li>
+<li>Ne pas introduire dans le local de matériel dangereux ou illicite, ou des personnes susceptibles de
+troubler l'activité de la MDE ou de porter préjudice à son matériel,</li>
+<li>Respecter les biens et les moyens collectifs mis à disposition, ne pas les détériorer,</li>
+<li>Rendre en état les salles du bâtiment A pour permettre aux cours de se dérouler sans problème,</li>
+<li>Signaler, dans les meilleurs délais, au BDE-UTC (MDE, local E300 – 03 44 23 43 71), et à
+l’administration (contacter le gardien de l’UTC) tout problème relatif aux locaux ou à la sécurité,</li>
+</ul></p>
+
+<p>D’autre part, l’étudiant présent dans la MDE en dehors des horaires d’ouverture libre doit veiller à sa
+sécurité. En effet, le BDE-UTC n’assure pas de permanence durant ces périodes. Il doit avoir conscience du
+privilège de pouvoir accéder à la MDE en dehors des accès libres face à d’autres étudiants à l’accès limité,
+et doit par conséquent éviter tout incident qui pourrait entraîner la fermeture de la MDE.</p>
+
   <div class="well">
     <form method="post" action="<?php echo url_for('locaux_create') ?>">
       <?php echo $form->renderHiddenFields(false) ?>

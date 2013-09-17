2f3ac1c4e4712968381fde5f838d0c708e7d5f71
Arthur Puyou <arthur@puyou.fr>
2013-09-17 02:34:50 +0200

    Sidebar mes associations

--- apps/frontend/modules/asso/templates/_myAssos.php
+++ apps/frontend/modules/asso/templates/_myAssos.php
@@ -2,15 +2,22 @@
 <div id="my_assos">
   <h1 class="bulle">Mes associations</h1>
   <?php if($assos && $assos->count() > 0): ?>
-    <?php foreach($assos as $asso): ?>
-      <div class="my_asso">
-        <a href="<?php echo url_for('assos_show',$asso) ?>"><?php echo showThumb($asso->getLogo(), 'assos', array('width'=>32, 'height'=>32), 'center') ?></a>
-        <h2>
-          <a href="<?php echo url_for('assos_show',$asso) ?>"><?php echo $asso->getName() ?></a>
-        </h2>
-        <div class="barre"></div>
-      </div>
-    <?php endforeach; ?>
+      <ul class="thumbnails">
+      <?php foreach($assos as $asso): ?>
+      <li class="span2">
+        <div class="thumbnail">
+          <div class="media">
+              <a class="pull-left" href="<?php echo url_for('assos_show',$asso) ?>">
+                  <?php echo showThumb($asso->getLogo(), 'assos', array('width'=>32, 'height'=>32, 'class'=>'media-object'), 'center') ?>
+              </a>
+              <div class="media-body">
+                  <h2 class="media-heading"><a href="<?php echo url_for('assos_show',$asso) ?>"><?php echo $asso->getName() ?></a></h2>
+              </div>
+          </div>
+        </div>
+      </li>
+      <?php endforeach; ?>
+    </ul>
   <?php else: ?>
     <p>Vous ne participez à aucune association.</p>
   <?php endif; ?>

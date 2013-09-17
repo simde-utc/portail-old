7d592c3b15ae3a1f11c806c9346a24c01e190729
Arthur Puyou <arthur@puyou.fr>
2013-09-18 01:32:56 +0200

    Boîte Mes services

--- apps/frontend/modules/services/templates/_myServicesFavoris.php
+++ apps/frontend/modules/services/templates/_myServicesFavoris.php
@@ -1,15 +1,23 @@
-<div id="my_services">
+<?php use_helper('Thumb') ?>
+<div id="my_assos">
   <h1 class="bulle">Services Favoris</h1>
   <?php if (count($services) > 0): ?>
-    <?php foreach ($services as $service): ?>
-      <div class="my_asso">
-        <a href="<?php echo $service->getService()->getUrl() ?>"><?php echo showThumb($service->getService()->getLogo(), 'services', array('width'=>32, 'height'=>32), 'center') ?></a>
-        <h2>
-          <a href="<?php echo $service->getService()->getUrl() ?>"><?php echo $service->getService()->getNom() ?></a>
-        </h2>
-        <div class="barre"></div>
-      </div>
-    <?php endforeach; ?>
+    <ul class="thumbnails">
+      <?php foreach ($services as $service): ?>
+        <li class="span2">
+          <div class="thumbnail">
+            <div class="media">
+              <a class="pull-left" href="<?php echo $service->getService()->getUrl() ?>">
+                <?php echo showThumb($service->getService()->getLogo(), 'assos', array('width'=>32, 'height'=>32, 'class'=>'media-object'), 'center') ?>
+              </a>
+              <div class="media-body">
+                <h2 class="media-heading"><a href="<?php echo $service->getService()->getUrl() ?>"><?php echo $service->getService()->getNom() ?></a></h2>
+              </div>
+            </div>
+          </div>
+        </li>
+      <?php endforeach; ?>
+    </ul>
   <?php else: ?>
     <p>Vous n'avez aucun service favori.</p>
   <?php endif; ?>

4ef99d7b80b41079eefd2eea2bc7d81431618baf
Arthur Puyou <arthur@puyou.fr>
2013-11-08 15:39:54 +0100

    Flux en taille de contenu

--- apps/frontend/modules/abonnement/templates/_myFlux.php
+++ apps/frontend/modules/abonnement/templates/_myFlux.php
@@ -3,18 +3,17 @@
   <?php if($abonnements && $abonnements->count() > 0): ?>
     <?php foreach ($abonnements as $abonnement):?>
       <?php if($abonnement['article'] == 'event'): ?>
-        <div class="events_abonnements">
+        <p class="events_abonnements">
           <b><a href="<?php echo url_for('event/show?id='.$abonnement['id']) ?>"><?php echo $abonnement['assoName'].' : '.$abonnement['name'] ?></a></b>
           <?php echo $abonnement['summary'] ?>
-          <div class="barre"></div>
-        </div>
+        </p>
       <?php endif; ?>
       <?php if($abonnement['article'] == 'article'): ?>
-        <div class="articles_abonnements">
+        <p class="articles_abonnements">
           <b><a href="<?php echo url_for('article/show?id='.$abonnement['id']) ?>"><?php echo $abonnement['assoName'].' : '.$abonnement['name'] ?></a></b>
           <?php echo $abonnement['summary'] ?>
           <div class="barre"></div>
-        </div>
+        </p>
       <?php endif; ?>
     <?php endforeach; ?>
   <?php else: ?>

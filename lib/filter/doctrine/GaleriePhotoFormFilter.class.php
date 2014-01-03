42596d3df9865b4c6dc4be74b0783a7f3edcfc65
arribbar <bartychou@gmail.com>
2014-01-03 18:45:40 +0100

    Modèle pour les galeries photo

---
+++ lib/filter/doctrine/GaleriePhotoFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * GaleriePhoto filter form.
+ *
+ * @package    simde
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class GaleriePhotoFormFilter extends BaseGaleriePhotoFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/filter/doctrine/PhotoFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Photo filter form.
+ *
+ * @package    simde
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class PhotoFormFilter extends BasePhotoFormFilter
+{
+  public function configure()
+  {
+  }
+}

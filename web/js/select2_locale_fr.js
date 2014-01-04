033eb8c87f90bf716ad576464975543947bee33d
Renan LE CARO <renan.lecaro@gmail.com>
2014-01-04 18:07:18 +0100

    minified js and css of select2

--- web/js/select2_locale_fr.js
+++ web/js/select2_locale_fr.js
@@ -1,15 +1,1 @@
-/**
- * Select2 French translation
- */
-(function ($) {
-    "use strict";
-
-    $.extend($.fn.select2.defaults, {
-        formatNoMatches: function () { return "Aucun résultat trouvé"; },
-        formatInputTooShort: function (input, min) { var n = min - input.length; return "Merci de saisir " + n + " caractère" + (n == 1? "" : "s") + " de plus"; },
-        formatInputTooLong: function (input, max) { var n = input.length - max; return "Merci de supprimer " + n + " caractère" + (n == 1? "" : "s"); },
-        formatSelectionTooBig: function (limit) { return "Vous pouvez seulement sélectionner " + limit + " élément" + (limit == 1 ? "" : "s"); },
-        formatLoadMore: function (pageNumber) { return "Chargement de résultats supplémentaires..."; },
-        formatSearching: function () { return "Recherche en cours..."; }
-    });
-})(jQuery);
+!function(e){"use strict";e.extend(e.fn.select2.defaults,{formatNoMatches:function(){return"Aucun résultat trouvé"},formatInputTooShort:function(e,t){var r=t-e.length;return"Merci de saisir "+r+" caractère"+(1==r?"":"s")+" de plus"},formatInputTooLong:function(e,t){var r=e.length-t;return"Merci de supprimer "+r+" caractère"+(1==r?"":"s")},formatSelectionTooBig:function(e){return"Vous pouvez seulement sélectionner "+e+" élément"+(1==e?"":"s")},formatLoadMore:function(){return"Chargement de résultats supplémentaires..."},formatSearching:function(){return"Recherche en cours..."}})}(jQuery);

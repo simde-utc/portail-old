0f8ff5fdaaecc84658be65682f6486a55103585f
Florent Thévenet <florent@fthevenet.fr>
2014-02-11 23:58:58 +0100

    [Tréso/Documents] Filtrage par date d'ajout
    WIP
    Reste à faire un dropdown correct et à mettre le css à part

--- apps/treso/modules/document/templates/indexSuccess.php
+++ apps/treso/modules/document/templates/indexSuccess.php
@@ -27,6 +27,16 @@ foreach ($documents as $document) {
 <script type="text/javascript">
 function documentCtrl($scope) {
     $scope.documents = <?php echo json_encode($docs); ?>;
+    $scope.opened = false;
+    $scope.date = {start: 0, end: undefined};
+    $scope.rangeSelected = function() {
+      if ($scope.selectedRange == undefined)
+        return false;
+      return $scope.selectedRange.year != undefined;
+    }
+    $scope.$on('close', function(event) {
+      $scope.opened = false;
+    });
 }
 </script>
 
@@ -42,20 +52,81 @@ function documentCtrl($scope) {
 .table thead th {
   vertical-align: top;
 }
+.dropdown-menu {
+  padding: 4px;
+  -webkit-border-radius: 4px;
+  -moz-border-radius: 4px;
+  border-radius: 4px;
+  border: 1px solid #ddd !important;
+  -moz-box-shadow: 2px 2px 5px #888;
+  -webkit-box-shadow: 2px 2px 5px #888;
+}
+
+.date-range table {
+  width: 100%;
+}
+
+.date-range td button {
+  background-image: none;
+  background-color: transparent;
+  border-width: 0px;
+  text-shadow: none;
+  box-shadow: none;
+  width: 100%;
+}
+
+.date-range td button:hover {
+  background-color: #eeeeee;
+}
+
+.date-range td button.active {
+  background-color: #006dcc;
+  color: white;
+}
+
+.date-range h1 {
+  font-size: 20px;
+  font-weight: 500;
+}
+
+.date-range hr {
+  margin: 5px 0px;
+}
+
+.date-range .btn-group button {
+  width: 50%;
+}
+.date-range td, .date-range td:hover {
+  border: none;
+  background-color: white !important;
+}
+.date-range tr, .date-range tr:hover {
+  border: none;
+  background-color: white !important;
+}
 </style>
 
 <table class="table table-striped table-bordered">
   <thead>
     <tr>
-      <th>Date</span></th>
+      <th><div class="btn-group">
+            <button class="btn dropdown-toggle" ng-click="opened = !opened">
+              <span ng-if="!rangeSelected()">Date</span>
+              <span ng-if="rangeSelected()">{{ selectedRange.month }} {{ selectedRange.year }}</span>
+              <i class="caret"></i></button>
+            <div class="dropdown-menu" visible="opened" style="width: 300px;">
+              <portail-date-range header="Filtrer par date" selection="selectedRange" start="date.start" end="date.end"></portail-date-range>
+            </div>
+          </div>
+      </th>
       <th>Nom <div class="pull-right"><input ng-model="search.nom" type="text" placeholder="Recherche"/></div></th>
       <th>Type</th>
       <th>Transaction</th>
     </tr>
   </thead>
   <tbody>
-    <tr ng-repeat="document in documents | filter:search">
-      <td class="reduce">{{ document.date_ajout_affichable }}</td>
+    <tr ng-repeat="document in documents | filter:search | int_range:'date_ajout':date.start:date.end">
+      <td class="reduce">{{ document.date_ajout * 1000 | date:'d MMMM yyyy' }}</td>
       <td class="expand"><a href="{{ document.url }}" target="_blank">{{ document.nom }}</a></td>
       <td class="reduce">{{ document.type }}</td>
       <td class="reduce"><a ng-if="document.transaction != null" href="{{ document.transaction.url }}">{{ document.transaction.libelle }}</a></td>
--- web/js/treso/document/document-ctrl.js
+++ web/js/treso/document/document-ctrl.js
@@ -1,2 +1,225 @@
 'use strict';
 var documentApp = angular.module('DocumentApp', []);
+
+function isClear(val) {
+    return val == undefined || val == ''
+}
+
+documentApp.filter('int_range', function() {
+    return function(array, attr, start, end) {
+        if (!angular.isArray(array)) return array;
+
+        if (isClear(start) && isClear(end)) return array;
+
+        if (isClear(start))
+            start = -Infinity; // pas de filtre inférieur
+        else
+            start = parseInt(start);
+
+        if (isClear(end))
+            end = Infinity; // pas de limite supérieure
+        else
+            end = parseInt(end);
+
+        var filtered = [];
+        for ( var j = 0; j < array.length; j++) {
+            var value = array[j][attr];
+            if (value >= start && value <= end) {
+                filtered.push(array[j]);
+            }
+        }
+        return filtered;
+    }
+});
+
+documentApp.filter('unique', function() {
+    return function(array, attr) {
+        if (!angular.isArray(array)) return array;
+
+        var filtered = [];
+        for ( var j = 0; j < array.length; j++) {
+            var value = array[j][attr];
+            var found = false;
+            for ( var i = 0; i < filtered.length; i++) {
+                if (filtered[i] == value) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                filtered.push(value);
+            }
+        }
+        return filtered;
+    }
+});
+
+documentApp.filter('slice', function() {
+    return function(array, start, end) {
+        if (!angular.isArray(array)) return array;
+
+        return array.slice(start, end);
+    }
+});
+
+documentApp.filter('cut', function() {
+    var cache = {};
+    return function(array, len) {
+        if (!angular.isArray(array)) return array;
+
+        if (cache[array] != undefined && cache[array][len] != undefined)
+            return cache[array][len];
+
+        var alen = array.length;
+        var result = [];
+        for (var i = 0; i < alen; i+=len) {
+            result.push(array.slice(i, i + len));
+        }
+
+        if (cache[array] == undefined) 
+            cache[array] = {};
+        cache[array][len] = result;
+
+        return result;
+    }
+});
+
+documentApp.filter('capitalize', function() {
+    return function(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    };
+});
+
+documentApp.directive('portailDateRange', function($locale) {
+  return {
+    restrict: 'E',
+    replace: true,
+    template: '<div class="date-range">'+
+        '<h1>{{ header }}</h1>'+
+        '<hr/>'+
+        '<table>'+
+        '<tr>'+
+        '<td><button ng-click="yearsBefore()" title="Années précédentes" class="btn">&lt;</button></td>'+
+        '<td ng-repeat="year in years"><button ng-class="{active: selected.year == year}" ng-click="selectYear(year)" class="btn">{{ year }}</button></td>'+
+        '<td><button ng-click="yearsAfter()" title="Années suivantes" class="btn" ng-class="{disabled: includesCurrentYear()}">&gt;</button></td>'+
+        '</tr>'+
+        '</table>'+
+        '<table>'+
+        '<tr ng-repeat="months_part in months | cut:4">'+
+        '<td ng-repeat="month in months_part"><button class="btn" ng-class="{active: selected.month == month}" ng-click="selectMonth(month)">{{ month | capitalize }}</button></td>'+
+        '</tr>'+
+        '</table>'+
+        '<hr/>'+
+        '<div class="btn-group">'+
+        '<button class="btn" ng-click="ok()">Ok</button>'+
+        '<button class="btn" ng-click="reset()">Annuler</button>'+
+        '</div>'+
+        '</div>',
+    scope: {
+      start: '=',
+      end: '=',
+      selected: '=selection',
+      header: '@'
+    },
+    controller: function($scope) {
+        $scope.genYears = function(max) {
+            $scope.years = [];
+            for (var i = $scope.nbYears - 1; i >= 0; i--)
+                $scope.years.push(max - i);
+        };
+
+        $scope.selected = {year: undefined, month: undefined};
+        $scope.today = new Date();
+        $scope.nbYears = 3;
+        $scope.genYears($scope.today.getFullYear());
+
+        $scope.months = $locale.DATETIME_FORMATS.MONTH;
+
+        // renvoie true si l'interval d'années contient l'année actuelle
+        $scope.includesCurrentYear = function() {
+            return $scope.years.indexOf($scope.today.getFullYear()) > -1;
+        }
+        $scope.yearsBefore = function() {
+            $scope.genYears($scope.years[0]);
+        };
+        $scope.yearsAfter = function() {
+            if ($scope.years[$scope.nbYears - 1] == $scope.today.getFullYear())
+                return;
+
+            $scope.genYears($scope.years[$scope.nbYears - 1] + $scope.nbYears - 1);
+        };
+        $scope.selectYear = function(year) {
+            // si on clique sur l'année active on désactive le filtre
+            if ($scope.selected.year == year)
+                return $scope.reset();
+
+            // on annule d'abord la sélection du mois
+            $scope.selected.month = undefined;
+            $scope.selected.year = year;
+            $scope.computeLimits();
+        };
+        $scope.selectMonth = function(month) {
+            // si on lique sur le mois actif on désactive le filtre
+            if ($scope.selected.month == month) {
+                $scope.selected.month = undefined;
+                return $scope.computeLimits();
+            }
+
+            // si aucune année n'est sélectionnée on choisit l'année actuelle
+            if (isClear($scope.selected.year))
+                $scope.selected.year = $scope.today.getFullYear();
+
+            $scope.selected.month = month;
+            $scope.computeLimits();
+        };
+        $scope.reset = function() {
+            $scope.selected.year = undefined;
+            $scope.selected.month = undefined;
+            $scope.computeLimits();
+            $scope.$emit('close');
+        };
+        $scope.ok = function() {
+            $scope.$emit('close');
+        }
+        $scope.computeLimits = function() {
+            // calcule start et end à partir de selected
+            if ($scope.selected.year != undefined) {
+                var start, end;
+                if ($scope.selected.month != undefined) {
+                    // on sélectionne un seul mois
+                    var monthi = $scope.months.indexOf($scope.selected.month);
+                    start = new Date($scope.selected.year, monthi);
+                    end = new Date($scope.selected.year, monthi + 1);
+                } else {
+                    // on sélectionne une année entière
+                    start = new Date($scope.selected.year, 0);
+                    end = new Date($scope.selected.year + 1, 0);
+                }
+                $scope.start = start.getTime() / 1000;
+                $scope.end = end.getTime() / 1000 - 1;
+            } else {
+                $scope.start = undefined;
+                $scope.end = undefined;
+            }
+        }
+    }
+  }
+});
+
+documentApp.directive('visible', function() {
+    return {
+        restrict: 'A',
+        scope: {
+            'visible': '='
+        },
+        link: function(scope, element, attrs) {
+            scope.$watch('visible', function(newv, oldv) {
+                if (newv) {
+                    element.css('display', 'block');
+                } else {
+                    element.css('display', 'none');
+                }
+            });
+        }
+    }
+});

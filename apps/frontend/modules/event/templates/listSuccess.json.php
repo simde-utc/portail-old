c0cd29f09a202dd8f4b423e245a80fec6e039d85
Hugo Rodde <rodde.hugo@gmail.com>
2014-03-13 09:39:17 +0100

    Ajout d'attributs dans le tableau de réponse
    Histoire d'avoir toutes les informations dans le JSON

--- apps/frontend/modules/event/templates/listSuccess.json.php
+++ apps/frontend/modules/event/templates/listSuccess.json.php
@@ -8,7 +8,11 @@ foreach ($events as $event){
     "end" => (strtotime($event->getEndDate())),
     "url" => url_for('event_show',$event),
     "color" => ($event->getAsso()->isPole())?($event->getAsso()->getPoleInfos()->getCouleur()) :($event->getAsso()->getPole()->getCouleur()),
-    "allDay" => (strtotime($event->getEndDate()) - strtotime($event->getStartDate()) > 24*3600) ? true : false
+    "allDay" => (strtotime($event->getEndDate()) - strtotime($event->getStartDate()) > 24*3600) ? true : false,
+    "summary" => truncate_text($event->getDescription(), 256),
+    "type" => ($event->getType()->getName()),
+    "formated_date" => format_date($event->getStartDate(), 'f', 'fr') . " - " . format_date($event->getEndDate(), 'f', 'fr'),
+    "asso" => event_from_asso_list($event)
   );  
 }
 echo json_encode($result);

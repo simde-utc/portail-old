96bc47869d29f9640e1e3f32b14af6490f23887d
Mel-BR <melissa.benali.richard@gmail.com>
2014-04-08 15:47:10 +0200

    Ajout onglet réservation

---
+++ apps/frontend/modules/reservation/actions/components.class.php
@@ -0,0 +1,18 @@
+<?php
+
+class reservationComponents extends sfComponents
+{
+
+  
+    public function executeCalendarMenu()
+  {
+    /*$assos = AssoTable::getInstance()->getAssosAndNotPolesList();
+
+    foreach($assos as $asso)
+    {
+      $poles[$asso->getPoleId()][] = $asso;
+    }
+    $this->poles = $poles;*/
+  }
+}
+
---
+++ apps/frontend/modules/reservation/templates/_calendarMenu.php
@@ -0,0 +1,13 @@
+<div id="calendarmenu">
+  <div class="row-fluid">
+    <?php $first = " offset1" ?>
+      <div class="bm_pole span2<?php echo $first; $first = ""; ?>">
+        <ul>
+            <li>
+              <a href="<?php echo url_for('agenda_detail') ?>">Calendrier des évènements</a> 
+              <a href="#">Calendrier des réservations</a>
+            </li>
+        </ul>
+      </div>
+  </div>
+</div>

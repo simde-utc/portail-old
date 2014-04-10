278d4ecf01746c25b2ae689406bc939ee27ec529
Mel-BR <melissa.benali.richard@gmail.com>
2014-04-10 12:12:50 +0200

    Modification CSS pour sous-menu calendrier

--- apps/frontend/modules/reservation/templates/_calendarMenu.php
+++ apps/frontend/modules/reservation/templates/_calendarMenu.php
@@ -1,12 +1,9 @@
 <div id="calendarmenu">
   <div class="row-fluid">
     <?php $first = " offset1" ?>
-      <div class="bm_pole span2<?php echo $first; $first = ""; ?>">
         <ul>
-            <li>
-              <a href="<?php echo url_for('agenda_detail') ?>">Calendrier des évènements</a> 
-              <a href="#">Calendrier des réservations</a>
-            </li>
+           <li><a href="<?php echo url_for('agenda_detail') ?>">Calendrier des évènements</a></li> 
+           <li><a href="#">Calendrier des réservations</a></li>
         </ul>
       </div>
   </div>

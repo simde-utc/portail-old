47fd6f3c7f921dc50f652411365f7f09653e0cf2
leganto <anthony.legiret@gmail.com>
2014-04-24 13:20:38 +0200

    Modification de la selection des salles (trier par pole)

--- apps/frontend/modules/reservation/templates/_listeSalles.php
+++ apps/frontend/modules/reservation/templates/_listeSalles.php
@@ -1,14 +1,24 @@
 <fieldset><legend>Custom Calendar</legend>
 	<form id="listSalle">
-		<?php if ($salles): ?>
+		<?php if (count($salles) > 0): ?>
 			<select onchange="this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value);">
+				<? $lastIdPole = -1 ?>
 				<?php foreach ($salles as $salle): ?>
-
+				
+					<?php if ($lastIdPole != $salle->getPole()->getId()): ?>
+					
+						<option value="">-----<?php echo $salle->getPole() ?>-----</option>
+					
+					<?php endif ?>
+					
 					<?php if ($salle->getID() == $idSalle) : ?>
 						<option selected="true" value="<?php echo url_for('/reservation/'.$salle->getID()) ?>"><?php echo $salle->getName() ?></option>
 					<?php else : ?>
 						<option value="<?php echo url_for('/reservation/'.$salle->getID()) ?>"><?php echo $salle->getName() ?></option>
-				<?php endif ?>
+					<?php endif ?>
+				
+					<?php $lastIdPole = $salle->getPole()->getId() ?>
+				
 				<?php endforeach ?>
 			</select>
 		<?php endif ?>

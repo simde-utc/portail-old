cbb274af377f7a27a7a025116fea47d792c3ae67
Mel-BR <melissa.benali.richard@gmail.com>
2014-04-25 19:55:18 +0200

    Premier test formulaire

--- apps/frontend/modules/reservation/templates/indexSuccess.php
+++ apps/frontend/modules/reservation/templates/indexSuccess.php
@@ -6,6 +6,49 @@
 
 <h1 class="partie">Calendrier des reservations de salles <i id="loading" class="fa fa-refresh fa-spin fa-lg pull-right"></i></h1>
 
+<div id="exemple_fadein" style="display:none; z-index:10001; position: relative; height: 600px; width: 300px; background-color:LightYellow; ">
+<form action="toto.php" method="post" >
+
+<fieldset>
+ <legend> Réservation: </legend>
+    <p><b>Salle FE302 Jeudi 18 Mai de 8h à 10h </b></p>
+    <p>Type de salle {Réunion | Musique | Salle des pôles}<br/><br/></p>
+    <label for="email">Login :</label>
+    <input type="email" name="email" size="20" maxlength="40" value="Automatique" id="email" />
+      <label for="utilise">Nom de l'asso: </label>
+   <select name="utilise" id="utilise">
+    <option value="toujours"> Chargement</option>
+    <option value="parfois"> Automatique</option>
+    <option value="jamais"> Des associations de la personne</option>
+   </select>
+     <br/> 
+     <p> Pôle concerné : XXX </p>
+    
+    <label for="email">Nombre de personnes:</label>
+    <input type="email" name="email" size="20" maxlength="40" value="Automatique" id="email" />
+    
+    <label for="utilise">Activité: </label>
+   <select name="utilise" id="utilise">
+    <option value="toujours"> Liste</option>
+    <option value="parfois"> à déterminer</option>
+    <option value="jamais"> par BDE</option>
+   </select>
+
+  <label for="comments">Vos commentaires :</label>
+   <textarea name="comments" id="comments" cols="20" rows="4">
+   </textarea>
+</fieldset>
+
+ <p>
+ <input type="submit" value="Envoyer" />
+ <input type="button" value="Annuler" onclick="$('#exemple_fadein').fadeOut();" />
+ </p>
+
+</form>
+</div>
+
+
+
 <?php include_component('reservation','listeSalles', array('idSalle' => $idSalle)) ?>
 
 <div id="calendar"></div>
@@ -15,7 +58,8 @@ $(document).ready(function() {
     
   	 dayClick: function(e)
   	 {
-  	 	alert("A day has been clicked" + e);
+  	 	//alert("A day has been clicked" + e);
+  	 	$('#exemple_fadein').fadeIn();
   	 },
   	 
     header: {

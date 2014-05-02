95eb60c183320a1264339e8e8ac523f4a1d03577
martinni <nina.utc@gmail.com>
2014-05-02 16:54:24 +0200

    ajout couleurs

--- apps/frontend/modules/reservation/templates/listSuccess.json.php
+++ apps/frontend/modules/reservation/templates/listSuccess.json.php
@@ -8,7 +8,7 @@ foreach ($reservations as $res){
     "allDay" => false,
     "start" => strtotime($res->getDate() . " " . $res->getheuredebut()),
     "end" => strtotime($res->getDate() . " " . $res->getheurefin()),
-    //"color" => ($res->getAsso()->isPole())?($res->getAsso()->getPoleInfos()->getCouleur()) :($res->getAsso()->getPole()->getCouleur()),
+    "color" => ($res->getAsso()->isPole())?($res->getAsso()->getPoleInfos()->getCouleur()) :($res->getAsso()->getPole()->getCouleur()),
   );  
 }
 echo json_encode($result);

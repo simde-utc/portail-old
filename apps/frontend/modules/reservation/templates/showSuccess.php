6eb4fe65a611474d60de36a7e781c2f573c3f24c
martinni <nina.utc@gmail.com>
2014-05-22 11:02:46 +0200

    ajout template recapitulatif

---
+++ apps/frontend/modules/reservation/templates/showSuccess.php
@@ -0,0 +1,17 @@
+<?php use_helper('Date') ?>
+<?php //use_helper('Thumb') ?>
+<?php //use_helper('Events') ?>
+
+
+<div class="part" id="reservation">
+  <h1>
+    <?php echo $reservation->getAsso() ?>
+  </h1>
+  <div class="row-fluid">
+        <p><i class="fa fa-calendar fa-2x fa-fw"></i>Le <?php echo format_date($reservation->getDate(), 'D', 'fr'); ?></p>
+	<p><i class="fa fa-clock-o fa-2x fa-fw"></i>De <?php echo format_date($reservation->getHeuredebut(), 't', 'fr') ?> à <?php echo format_date($reservation->getHeurefin(), 't', 'fr') ?></p>
+	<p><i class="fa fa-map-marker fa-2x fa-fw"></i><?php echo ucfirst($reservation->getSalle()); ?></p>
+	<p><i class="fa fa-home fa-2x fa-fw"></i>Créé par <?php echo $reservation->getIdUserReserve(); ?> pour <a href="<?php echo url_for('assos_show', array('login' => $reservation->getAsso()->getLogin()))?>"><?php echo ucfirst($reservation->getAsso()) ;?></a></p>
+  </div>
+
+</div>

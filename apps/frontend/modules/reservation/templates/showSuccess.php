c5df2cbbe32418cd127386793d37fe63067d5412
martinni <nina.utc@gmail.com>
2014-05-29 12:38:27 +0200

    modification recapitulatif

--- apps/frontend/modules/reservation/templates/showSuccess.php
+++ apps/frontend/modules/reservation/templates/showSuccess.php
@@ -5,13 +5,16 @@
 
 <div class="part" id="reservation">
   <h1>
-    <?php echo $reservation->getAsso() ?>
+    <?php if($reservation->getAsso()->getId()) echo $reservation->getAsso(); 
+	else echo $reservation->getUserReserve()->getName();?>
   </h1>
   <div class="row-fluid">
         <p><i class="fa fa-calendar fa-2x fa-fw"></i>Le <?php echo format_date($reservation->getDate(), 'D', 'fr'); ?></p>
 	<p><i class="fa fa-clock-o fa-2x fa-fw"></i>De <?php echo format_date($reservation->getHeuredebut(), 't', 'fr') ?> à <?php echo format_date($reservation->getHeurefin(), 't', 'fr') ?></p>
 	<p><i class="fa fa-map-marker fa-2x fa-fw"></i><?php echo ucfirst($reservation->getSalle()); ?></p>
-	<p><i class="fa fa-home fa-2x fa-fw"></i>Créé par <a href="<?php echo url_for('profile_show', array('username' => $reservation->getUserReserve()->getUsername() ))?>"><?php echo $reservation->getUserReserve()->getUsername(); ?></a> pour <a href="<?php echo url_for('assos_show', array('login' => $reservation->getAsso()->getLogin()))?>"><?php echo ucfirst($reservation->getAsso()) ;?></a></p>
+	<p><i class="fa fa-home fa-2x fa-fw"></i>Créé par <a href="<?php echo url_for('profile_show', array('username' => $reservation->getUserReserve()->getUsername() ))?>"><?php echo $reservation->getUserReserve()->getUsername(); ?></a>
+	<?php 
+		if($reservation->getAsso()->getId()) echo  " pour le <a href=\"". url_for('assos_show', array('login' => $reservation->getAsso()->getLogin())) . "\">" . ucfirst($reservation->getAsso()) ."</a>" ; ?></p>
   </div>
 
 </div>

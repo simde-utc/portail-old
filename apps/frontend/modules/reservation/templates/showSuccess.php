93e0cfbe4378fa51264420304d78a7bc39862496
martinni <nina.utc@gmail.com>
2014-06-13 00:05:49 +0200

    correction recapitulatif allDay

--- apps/frontend/modules/reservation/templates/showSuccess.php
+++ apps/frontend/modules/reservation/templates/showSuccess.php
@@ -10,7 +10,11 @@
   </h1>
   <div class="row-fluid">
         <p><i class="fa fa-calendar fa-2x fa-fw"></i>Le <?php echo format_date($reservation->getDate(), 'D', 'fr'); ?></p>
-	<p><i class="fa fa-clock-o fa-2x fa-fw"></i>De <?php echo format_date($reservation->getHeuredebut(), 't', 'fr') ?> à <?php echo format_date($reservation->getHeurefin(), 't', 'fr') ?></p>
+	<?php if($reservation->getAllday()) : ?>
+		<p><i class="fa fa-clock-o fa-2x fa-fw"></i>Toute la journée</p>
+	<?php else : ?>
+		<p><i class="fa fa-clock-o fa-2x fa-fw"></i>De <?php echo format_date($reservation->getHeuredebut(), 't', 'fr') ?> à <?php echo format_date($reservation->getHeurefin(), 't', 'fr') ?></p>
+	<?php endif; ?>
 	<p><i class="fa fa-tag fa-2x fa-fw"></i><em><?php echo $reservation->getActivite(); ?></em></p>
 	<p><i class="fa fa-home fa-2x fa-fw"></i><?php echo ucfirst($reservation->getSalle()); ?></p>
 	<p><i class="fa fa-info-circle fa-2x fa-fw"></i>Créé par <a href="mailto:<?php echo $reservation->getUserReserve()->getEmailAddress() ?>"><?php echo $reservation->getUserReserve()->getUsername() ?></a>

8d5256d8e02a5414737d5b8ac83905eaaa3d58c3
leganto <anthony.legiret@gmail.com>
2014-06-16 22:38:36 +0200

    Petite modif

--- apps/bde/modules/reservation/templates/salleSuccess.php
+++ apps/bde/modules/reservation/templates/salleSuccess.php
@@ -3,14 +3,14 @@
 <h4><?php echo link_to ('Nouvelle salle', 'reservation_salle_new') ?></h4>
 
 <h3>Liste des salles</h3>
-
+<div id="table_salle">
 <?php foreach ($salles as $salle): ?>
 
 	<p>
-		<span style="margin-right:3em;"><?php echo ucfirst ($salle->getName()) ?></span>
-		<span style="margin-right:3em;"><?php echo link_to ('Edit', 'reservation_salle_update', array ('id' => $salle->getId())) ?></span>
-		<span style="margin-right:3em;"><?php echo link_to ('Delete', 'reservation_salle_delete', array ('id' => $salle->getId())) ?></span>
+		<span class="name"><?php echo ucfirst ($salle->getName()) ?></span>
+		<span><?php echo link_to ('Edit', 'reservation_salle_update', array ('id' => $salle->getId())) ?></span>
+		<span><?php echo link_to ('Delete', 'reservation_salle_delete', array ('id' => $salle->getId())) ?></span>
 	</p>
-
+	
 <?php endforeach; ?>
-
+</div>

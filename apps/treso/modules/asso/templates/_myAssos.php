9dc56efb40fb4299cb13a33cd7440368340d38f1
Florent Thévenet <florent@fthevenet.fr>
2014-09-13 21:23:36 +0200

    [Treso] Layout plus compact
    La liste des assos est dans un dropdown
    la liste des modules pour l'asso en cours est dans la barre du haut
    ça permet de donner toute la largeur au vrai contenu

--- apps/treso/modules/asso/templates/_myAssos.php
+++ apps/treso/modules/asso/templates/_myAssos.php
@@ -1,11 +1,27 @@
 <?php if($assos && $assos->count() > 0): ?>
-  <div class="nav-collapse">
-    <ul class="nav">
-      <?php foreach($assos as $asso): ?>
-        <li class="<?php if(!empty($current_asso) && ($current_asso->getId() == $asso->getId())): ?>active<?php endif ?>">
-            <a href="<?php echo url_for('budget_list', $asso) ?>"><?php echo $asso->getName() ?></a>
-        </li>
-      <?php endforeach; ?>
-    </ul>
-  </div>
+  <ul class="nav">
+    <li class="dropdown">
+      <a class="dropdown-toggle brand" data-toggle="dropdown" href="<?php echo url_for('homepage') ?>">
+        <?php if($current_asso): ?>
+          Tréso <?php echo $current_asso->getName(); ?>
+        <?php else: ?>
+          BDE-UTC - Outils de Trésorerie
+        <?php endif; ?>
+        <b class="caret"></b>
+      </a>
+      <ul class="dropdown-menu">
+        <?php foreach($assos as $asso): ?>
+          <?php if(empty($current_asso) || ($current_asso->getId() != $asso->getId())): ?>
+          <li>
+              <a href="<?php echo url_for('budget_list', $asso) ?>"><?php echo $asso->getName() ?></a>
+          </li>
+          <?php endif; ?>
+        <?php endforeach; ?>
+        <li class="divider"></li>
+        <li><a href="<?php echo url_for('homepage') ?>">Accueil</a></li>
+      </ul>
+    <li>
+  </ul>
+<?php else: ?>
+  <a class="brand" href="<?php echo url_for('homepage') ?>">BDE-UTC - Outils de Trésorerie</a>
 <?php endif ?>
